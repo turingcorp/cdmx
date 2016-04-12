8aedb98ad386b39ebd895abaaf6ec3d795a02b14
vauxhall <zero.griffin@gmail.com>
2016-04-12 12:56:48 -0500

    menu button in bar air

--- cdmx/sourceteq/source/v/air/vairbar.m
+++ cdmx/sourceteq/source/v/air/vairbar.m
@@ -1,5 +1,6 @@
 #import "vairbar.h"
 #import "uicolor+uicolormain.h"
+#import "vmenubutton.h"
 
 static NSUInteger const navbarintrinsicwidth = 320;
 
@@ -13,6 +14,15 @@ static NSUInteger const navbarintrinsicwidth = 320;
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     self.controller = controller;
     
+    vmenubutton *menubutton = [[vmenubutton alloc] init];
+    
+    [self addSubview:menubutton];
+    
+    NSDictionary *views = @{@"menubutton":menubutton};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[menubutton]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[menubutton]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
