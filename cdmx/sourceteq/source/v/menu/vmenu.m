2b61f9419734c6a070bed381dba71e6dc9d924ca
vauxhall <zero.griffin@gmail.com>
2016-04-12 13:12:23 -0500

    banner height

--- cdmx/sourceteq/source/v/menu/vmenu.m
+++ cdmx/sourceteq/source/v/menu/vmenu.m
@@ -2,6 +2,8 @@
 #import "uicolor+uicolormain.h"
 #import "vmenubanner.h"
 
+static NSUInteger const bannerheight = 200;
+
 @implementation vmenu
 
 -(instancetype)init:(cmenu*)controller
@@ -10,6 +12,16 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor collection]];
     self.controller = controller;
+    
+    vmenubanner *banner = [[vmenubanner alloc] init];
+    
+    [self addSubview:banner];
+    
+    NSDictionary *views = @{@"banner":banner};
+    NSDictionary *metrics = @{@"bannerheight":@(bannerheight)};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[banner]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[banner(bannerheight)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
