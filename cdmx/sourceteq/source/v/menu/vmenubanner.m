06cf52eed3943ebe66755dfcb40d6821c29e02a1
vauxhall <zero.griffin@gmail.com>
2016-04-12 15:21:36 -0500

    map in banner

--- cdmx/sourceteq/source/v/menu/vmenubanner.m
+++ cdmx/sourceteq/source/v/menu/vmenubanner.m
@@ -8,6 +8,22 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor whiteColor]];
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self setUserInteractionEnabled:NO];
+    
+    UIImageView *background = [[UIImageView alloc] init];
+    [background setUserInteractionEnabled:NO];
+    [background setClipsToBounds:YES];
+    [background setImage:[UIImage imageNamed:@"general_map"]];
+    [background setContentMode:UIViewContentModeScaleAspectFit];
+    [background setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:background];
+    
+    NSDictionary *views = @{@"background":background};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[background]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[background]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
