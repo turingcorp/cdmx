f15e64ddf1305cfcc326f2f01649f9b2e9f89167
vauxhall <zero.griffin@gmail.com>
2016-04-12 08:36:55 -0500

    view loading

---
+++ cdmx/sourceteq/source/v/main/vloading.m
@@ -0,0 +1,14 @@
+#import "vloading.h"
+
+@implementation vloading
+
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    
+    return self;
+}
+
+@end
