2ce07316891f65495579a8c6eb7e7ac401555e8e
vauxhall <zero.griffin@gmail.com>
2016-04-12 11:52:09 -0500

    model pages

---
+++ cdmx/sourceteq/source/m/pages/mpagessectionitem.h
@@ -0,0 +1,13 @@
+//
+//  mpagessectionitem.h
+//  cdmx
+//
+//  Created by zero on 4/12/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mpagessectionitem : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/m/pages/mpagessectionitem.m
@@ -0,0 +1,13 @@
+//
+//  mpagessectionitem.m
+//  cdmx
+//
+//  Created by zero on 4/12/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mpagessectionitem.h"
+
+@implementation mpagessectionitem
+
+@end
