58ad54227acc332c4f26847bf3cdacfcfe88a8b9
vauxhall <zero.griffin@gmail.com>
2016-04-12 13:03:13 -0500

    controller menu

--- cdmx/sourceteq/source/c/menu/cmenu.m
+++ cdmx/sourceteq/source/c/menu/cmenu.m
@@ -1,10 +1,19 @@
 #import "cmenu.h"
+#import "vmenu.h"
+
+@interface cmenu ()
+
+@property(strong, nonatomic)vmenu *view;
+
+@end
 
 @implementation cmenu
 
+@dynamic view;
+
 -(void)loadView
 {
-    self.view = [[vair alloc] init:self];
+    self.view = [[vmenu alloc] init:self];
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle
