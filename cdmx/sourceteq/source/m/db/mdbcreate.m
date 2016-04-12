0fc3b5d1b74fa62d03abc26c414e63620590f2ce
vauxhall <zero.griffin@gmail.com>
2016-04-12 18:03:27 -0500

    model db

--- cdmx/sourceteq/source/m/db/mdbcreate.m
+++ cdmx/sourceteq/source/m/db/mdbcreate.m
@@ -11,14 +11,16 @@
              @"CREATE TABLE station "
              "(id INTEGER PRIMARY KEY, stationid INTEGER, latitude INTEGER, longitude INTEGER, altitude INTEGER, shortname TEXT COLLATE NOCASE, name TEXT COLLATE NOCASE, message TEXT);"];
     [dbcon query:query];
+    
     [mdbcreate loadstations:dbcon];
     
     [dbcon commit];
+    
+    NSLog(@"%@", [db rows:@"select * from station"]);
 }
 
 +(void)loadstations:(db*)dbcon
 {
-    NSString *query;
     NSString *raw = [NSString stringWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"stations" withExtension:@"csv"] encoding:NSUTF8StringEncoding error:nil];
     NSArray *rawlines = [raw componentsSeparatedByString:@"\n"];
     NSUInteger countlines = rawlines.count;
@@ -31,10 +33,30 @@
         
         NSAssert(countrows == 7, @"Cannot load stations from csv file", raw);
         
-        NSUInteger rawstationid = [rawrows[0] unsignedIntegerValue];
+        NSUInteger rawstationid = [rawrows[6] integerValue];
         double rawlatitude = [rawrows[3] doubleValue];
         double rawlongitude = [rawrows[2] doubleValue];
+        NSUInteger rawaltitude = [rawrows[4] integerValue];
+        NSString *rawshortname = rawrows[0];
+        NSString *rawname = rawrows[1];
+        NSString *rawmessage = rawrows[5];
+        NSUInteger storinglatitude = rawlatitude * coordinatesmult;
+        NSUInteger storinglongitude = rawlongitude * coordinatesmult;
         
+        NSMutableString *query = [NSMutableString string];
+        [query appendString:@"INSERT INTO station "];
+        [query appendString:@"(stationid, latitude, longitude, altitude, shortname, name, message) "];
+        [query appendString:@"values("];
+        [query appendFormat:@"%@, ", @(rawstationid)];
+        [query appendFormat:@"%@, ", @(storinglatitude)];
+        [query appendFormat:@"%@, ", @(storinglongitude)];
+        [query appendFormat:@"%@, ", @(rawaltitude)];
+        [query appendFormat:@"\"%@\", ", rawshortname];
+        [query appendFormat:@"\"%@\", ", rawname];
+        [query appendFormat:@"\"%@\"", rawmessage];
+        [query appendString:@");"];
+        
+        [dbcon query:query];
     }
 }
 
