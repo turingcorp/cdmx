573c7fd2f3b49b5e4b138448089799f2190ac2ae
vauxhall <zero.griffin@gmail.com>
2016-04-12 17:21:45 -0500

    loading stations

--- cdmx/sourceteq/source/m/db/mdbcreate.m
+++ cdmx/sourceteq/source/m/db/mdbcreate.m
@@ -10,4 +10,23 @@
     [dbcon commit];
 }
 
++(void)loadstations:(db*)dbcon
+{
+    NSString *query;
+    NSString *raw = [NSString stringWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"stations" withExtension:@"csv"] encoding:NSUTF8StringEncoding error:nil];
+    NSArray *rawlines = [raw componentsSeparatedByString:@"\n"];
+    NSUInteger countlines = rawlines.count;
+    
+    for(NSUInteger i = 2; i < countlines; i++)
+    {
+        NSString *rawline = rawlines[i];
+        NSArray *rawrow = [rawline componentsSeparatedByString:@","];
+        NSUInteger countrows = rawrow.count;
+        
+        NSAssert(countrows == 7, @"Cannot load stations from csv file", raw);
+        
+        
+    }
+}
+
 @end
