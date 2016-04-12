0d6dcb299d989414340d49cff74efba2271a4020
vauxhall <zero.griffin@gmail.com>
2016-04-12 11:33:27 -0500

    controller air - view

--- cdmx/sourceteq/source/c/air/cair.m
+++ cdmx/sourceteq/source/c/air/cair.m
@@ -1,7 +1,19 @@
 #import "cair.h"
+#import "vair.h"
+
+@interface cair ()
+
+@property(strong, nonatomic)vair *view;
+
+@end
 
 @implementation cair
 
+@dynamic view;
 
+-(void)loadView
+{
+    self.view = [[vair alloc] init:self];
+}
 
 @end
