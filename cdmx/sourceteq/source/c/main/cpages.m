3b7471e29383cef70c3dfd19004c4e1ec9ea193e
vauxhall <zero.griffin@gmail.com>
2016-04-12 08:28:26 -0500

    controller pages

--- cdmx/sourceteq/source/c/main/cpages.m
+++ cdmx/sourceteq/source/c/main/cpages.m
@@ -1,13 +1,57 @@
-//
-//  cpages.m
-//  cdmx
-//
-//  Created by zero on 4/12/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "cpages.h"
 
 @implementation cpages
+
+-(instancetype)init
+{
+    self = [super initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    [self sectionhome:NO direction:UIPageViewControllerNavigationDirectionForward];
+    
+    return self;
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setEdgesForExtendedLayout:UIRectEdgeNone];
+    [self setExtendedLayoutIncludesOpaqueBars:NO];
+    [self setAutomaticallyAdjustsScrollViewInsets:NO];
+}
+
+-(BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+#pragma mark actions
+
+#pragma mark functionality
+
+-(void)changecontroller:(UIViewController*)controller direction:(UIPageViewControllerNavigationDirection)direction animated:(BOOL)animated
+{
+    NSArray *array = @[controller];
+    [self setViewControllers:array direction:direction animated:animated completion:nil];
+}
+
+#pragma mark public
+
+-(void)sectionhome:(BOOL)animated direction:(UIPageViewControllerNavigationDirection)direction
+{
+    UIViewController *controller = [[chome alloc] init];
+    [self changecontroller:controller direction:direction animated:animated];
+}
+
+-(void)sectiongame
+{
+    UIViewController *controller = [[cgameloader alloc] init];
+    [self changecontroller:controller direction:UIPageViewControllerNavigationDirectionForward animated:YES];
+}
+
+-(void)sectiongamestart:(mgame*)model
+{
+    UIViewController *controller = [[cgame alloc] init:model];
+    [self changecontroller:controller direction:UIPageViewControllerNavigationDirectionForward animated:YES];
+}
+
 
 @end
