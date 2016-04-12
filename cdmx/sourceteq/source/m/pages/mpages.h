30c1d9f087e211a5df1e4cb4970c26060544622e
vauxhall <zero.griffin@gmail.com>
2016-04-12 11:34:41 -0500

    model pages

---
+++ cdmx/sourceteq/source/m/pages/mpages.h
@@ -0,0 +1,13 @@
+//
+//  mpages.h
+//  cdmx
+//
+//  Created by zero on 4/12/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mpages : NSObject
+
+@end
