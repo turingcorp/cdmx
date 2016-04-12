561ed576f018f72f9d0c73882bf458c63e6c6d76
vauxhall <zero.griffin@gmail.com>
2016-04-12 12:34:32 -0500

    collection in air

--- cdmx/sourceteq/source/v/air/vair.h
+++ cdmx/sourceteq/source/v/air/vair.h
@@ -1,10 +1,11 @@
 #import "appdel.h"
 #import "cair.h"
 
-@interface vair:UIView
+@interface vair:UIView<UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 
 -(instancetype)init:(cair*)controller;
 
 @property(weak, nonatomic)cair *controller;
+@property(weak, nonatomic)UICollectionView *collection;
 
 @end
--- cdmx/sourceteq/source/v/air/vair.m
+++ cdmx/sourceteq/source/v/air/vair.m
@@ -12,13 +12,27 @@
     
     vairbar *bar = [[vairbar alloc] init];
     
-    [self addSubview:bar];
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
     
-    NSDictionary *views = @{@"bar":bar};
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection setClipsToBounds:YES];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setDataSource:self];
+    [collection setDelegate:self];
+    
+    [self addSubview:bar];
+    [self addSubview:collection];
+    
+    NSDictionary *views = @{@"bar":bar, @"col":collection};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]-0-[col]-0-|" options:0 metrics:metrics views:views]];
+
     
     return self;
 }
