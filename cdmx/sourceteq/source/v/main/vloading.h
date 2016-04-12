bce09cf4fa9010ebc36914822411c8e23d1ade6b
vauxhall <zero.griffin@gmail.com>
2016-04-12 10:55:27 -0500

    view spinner

--- cdmx/sourceteq/source/v/main/vloading.h
+++ cdmx/sourceteq/source/v/main/vloading.h
@@ -1,5 +1,9 @@
 #import <UIKit/UIKit.h>
+#import "vspinner.h"
 
 @interface vloading:UIView
+
+@property(weak, nonatomic)vspinner *spinner;
+@property(weak, nonatomic)NSLayoutConstraint *layoutmargintop;
 
 @end
--- cdmx/sourceteq/source/v/main/vloading.m
+++ cdmx/sourceteq/source/v/main/vloading.m
@@ -6,9 +6,32 @@
 {
     self = [super init];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor whiteColor]];
+    [self setBackgroundColor:[UIColor blueColor]];
+
+    vspinner *spinner = [[vspinner alloc] init];
+    self.spinner = spinner;
+    [self addSubview:spinner];
+    
+    NSDictionary *views = @{@"spinner":spinner};
+    NSDictionary *metrics = @{};
+    
+    self.layoutmargintop = [NSLayoutConstraint constraintWithItem:spinner attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1 constant:0];
+    [self addConstraint:self.layoutmargintop];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[spinner]-0-|" options:0 metrics:metrics views:views]];
+
     
     return self;
+}
+
+-(void)layoutSubviews
+{
+    CGFloat height = self.bounds.size.height;
+    CGFloat spinnerheight = self.spinner.bounds.size.height;
+    CGFloat remain = height - spinnerheight;
+    CGFloat margin = remain / 2.0;
+    self.layoutmargintop.constant = margin;
+    
+    [super layoutSubviews];
 }
 
 @end
