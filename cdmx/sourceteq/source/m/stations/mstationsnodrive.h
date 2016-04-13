7583a09694cdfb17f3f52eae2eac872f64d5a6a2
vauxhall <zero.griffin@gmail.com>
2016-04-13 18:29:05 -0500

    model

--- cdmx/sourceteq/source/m/stations/mstationsnodrive.h
+++ cdmx/sourceteq/source/m/stations/mstationsnodrive.h
@@ -1,12 +1,10 @@
 #import <UIKit/UIKit.h>
 #import "mstationsnodrivehologram.h"
-#import "mstationsnodrivecolor.h"
 #import "mstationsnodriveplate.h"
 
 @interface mstationsnodrive:NSObject
 
 @property(strong, nonatomic)NSArray<mstationsnodrivehologram*> *holograms;
-@property(strong, nonatomic)NSArray<mstationsnodrivecolor*> *colors;
 @property(strong, nonatomic)NSArray<mstationsnodriveplate*> *plates;
 @property(strong, nonatomic)NSDate *date;
 
