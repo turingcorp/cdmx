ad9effbc8b75f48d75a3a1edceaaa15ffe9d2b57
vauxhall <zero.griffin@gmail.com>
2016-04-13 09:43:58 -0500

    stations reading and reading item

---
+++ cdmx/sourceteq/source/m/stations/mstationsreading.m
@@ -0,0 +1,13 @@
+//
+//  mstationsreading.m
+//  cdmx
+//
+//  Created by zero on 4/13/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mstationsreading.h"
+
+@implementation mstationsreading
+
+@end
---
+++ cdmx/sourceteq/source/m/stations/mstationsreadingitem.h
@@ -0,0 +1,13 @@
+//
+//  mstationsreadingitem.h
+//  cdmx
+//
+//  Created by zero on 4/13/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mstationsreadingitem : NSObject
+
+@end
