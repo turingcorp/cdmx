758986c4292597d56a0bdb637f4d7bc5050591c3
vauxhall <zero.griffin@gmail.com>
2016-04-13 15:43:41 -0500

    model

--- cdmx/sourceteq/source/m/stations/mstationsreadingitem.h
+++ cdmx/sourceteq/source/m/stations/mstationsreadingitem.h
@@ -1,11 +1,15 @@
 #import <UIKit/UIKit.h>
 #import "mstationsreadingitemindex.h"
 #import "mstationsitemlocation.h"
+#import "mstationsreadingitemconditions.h"
+#import "mstationsreadingitemwind.h"
 
 @interface mstationsreadingitem:NSObject
 
 @property(strong, nonatomic)mstationsitemlocation *location;
 @property(strong, nonatomic)mstationsreadingitemindex *index;
+@property(strong, nonatomic)mstationsreadingitemconditions *conditions;
+@property(strong, nonatomic)mstationsreadingitemwind *wind;
 @property(copy, nonatomic)NSString *name;
 @property(copy, nonatomic)NSString *shortname;
 @property(copy, nonatomic)NSString *pollutant;
