5320224f0c0ae53307d669f1e1cdd26e64666402
vauxhall <zero.griffin@gmail.com>
2016-04-13 09:11:10 -0500

    a call

--- cdmx/sourceteq/source/a/call/acall.h
+++ cdmx/sourceteq/source/a/call/acall.h
@@ -4,17 +4,11 @@
 
 @interface acall:NSObject
 
--(instancetype)init:(id)variables;
 -(void)buildrequest;
--(void)buildendpoint:(NSDictionary*)params;
 -(NSURLRequest*)request;
 
 @property(strong, nonatomic)aparser *parser;
-@property(copy, nonatomic)id variables;
-@property(copy, nonatomic)NSString *server;
-@property(copy, nonatomic)NSString *endpoint;
-@property(copy, nonatomic)NSString *keyforserver;
-@property(copy, nonatomic)NSString *keyforendpoint;
+@property(copy, nonatomic)NSString *urlstring;
 @property(nonatomic)NSURLRequestCachePolicy cachepolicy;
 @property(nonatomic)NSUInteger timeout;
 @property(nonatomic)BOOL post;
--- cdmx/sourceteq/source/a/call/acall.m
+++ cdmx/sourceteq/source/a/call/acall.m
@@ -3,15 +3,12 @@
 
 @implementation acall
 
--(instancetype)init:(id)variables
+-(instancetype)init
 {
     self = [super init];
     
-    self.variables = variables;
     self.post = NO;
     self.timeout = 25;
-    self.keyforserver = @"server";
-    self.keyforendpoint = @"";
     self.cachepolicy = NSURLRequestReloadIgnoringLocalCacheData;
     
     return self;
@@ -21,47 +18,17 @@
 
 -(void)buildrequest
 {
-    NSString *filepath = [[NSBundle mainBundle] pathForResource:@"urlpar" ofType:@"plist"];
-    NSURL *fileurl = [NSURL fileURLWithPath:filepath];
-    NSDictionary *params = [NSDictionary dictionaryWithContentsOfURL:fileurl];
-    self.server = params[self.keyforserver];
-    [self buildendpoint:params];
-}
-
--(void)buildendpoint:(NSDictionary*)params
-{
-    self.endpoint = [NSString stringWithFormat:@"%@?", params[self.keyforendpoint]];
+    self.urlstring = @"";
 }
 
 -(NSURLRequest*)request
 {
-    NSMutableURLRequest *request;
-    NSMutableString *urlstring = [NSMutableString string];
-    NSURL *url;
-    [urlstring appendString:self.server];
-    [urlstring appendString:self.endpoint];
-    
-    if(!self.post)
-    {
-        [urlstring appendString:self.variables];
-    }
-    
-    url = [NSURL URLWithString:urlstring];
-    request = [NSMutableURLRequest requestWithURL:url cachePolicy:self.cachepolicy timeoutInterval:self.timeout];
-    
-    if(self.post)
-    {
-        NSData *bodydata = [NSJSONSerialization dataWithJSONObject:self.variables options:0 error:nil];
-        
-        [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-        [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setHTTPMethod:@"POST"];
-        [request setHTTPBody:bodydata];
-    }
+    NSURL *url = [NSURL URLWithString:self.urlstring];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:self.cachepolicy timeoutInterval:self.timeout];
     
 #if DEBUG
     
-    NSLog(@"%@", urlstring);
+    NSLog(@"%@", self.urlstring);
     
 #endif
     
