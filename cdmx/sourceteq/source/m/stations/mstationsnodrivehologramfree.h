b7073a099462d3a9575a4f39388871d58526828d
vauxhall <zero.griffin@gmail.com>
2016-04-13 18:32:56 -0500

    models

--- cdmx/sourceteq/source/m/stations/mstationsnodrivehologramfree.h
+++ cdmx/sourceteq/source/m/stations/mstationsnodrivehologramfree.h
@@ -1,13 +1,6 @@
-//
-//  mstationsnodrivehologramfree.h
-//  cdmx
-//
-//  Created by zero on 4/13/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import <UIKit/UIKit.h>
+#import "mstationsnodrivehologram.h"
 
-#import <Foundation/Foundation.h>
-
-@interface mstationsnodrivehologramfree : NSObject
+@interface mstationsnodrivehologramfree:mstationsnodrivehologram
 
 @end
