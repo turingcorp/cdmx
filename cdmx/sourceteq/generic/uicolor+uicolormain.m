81a90f60e08f8433c5fca3267a5c463bf92649a6
vauxhall <zero.griffin@gmail.com>
2016-04-13 10:55:32 -0500

    colors

--- cdmx/sourceteq/generic/uicolor+uicolormain.m
+++ cdmx/sourceteq/generic/uicolor+uicolormain.m
@@ -19,27 +19,27 @@
 
 +(UIColor*)uvgreen
 {
-    return [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    return [UIColor colorWithRed:0.3 green:1 blue:0.5 alpha:1];
 }
 
 +(UIColor*)uvyellow
 {
-    return [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    return [UIColor colorWithRed:0.94 green:0.9 blue:0.1 alpha:1];
 }
 
 +(UIColor*)uvorange
 {
-    return [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    return [UIColor colorWithRed:1 green:0.7 blue:0 alpha:1];
 }
 
 +(UIColor*)uvred
 {
-    return [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    return [UIColor colorWithRed:1 green:0 blue:0 alpha:1];
 }
 
 +(UIColor*)uvviolet
 {
-    return [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    return [UIColor colorWithRed:0.8 green:0.4 blue:0.6 alpha:1];
 }
 
 @end
