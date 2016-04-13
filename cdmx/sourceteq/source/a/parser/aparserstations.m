91dd5d77d92c7db49b9b9d30a7cd1437f75be944
vauxhall <zero.griffin@gmail.com>
2016-04-13 09:25:39 -0500

    parser stations

--- cdmx/sourceteq/source/a/parser/aparserstations.m
+++ cdmx/sourceteq/source/a/parser/aparserstations.m
@@ -2,4 +2,14 @@
 
 @implementation aparserstations
 
+-(void)parse:(NSDictionary*)json
+{
+    [super parse:json];
+    
+    if(self.validjson)
+    {
+        NSLog(@"%@", self.validjson);
+    }
+}
+
 @end
