b945e5f0dc120c61fccc83d811e7eab1f8edcb58
vauxhall <zero.griffin@gmail.com>
2016-04-13 10:01:38 -0500

    model no drive

--- cdmx/sourceteq/source/m/stations/mstationsreading.m
+++ cdmx/sourceteq/source/m/stations/mstationsreading.m
@@ -2,4 +2,11 @@
 
 @implementation mstationsreading
 
+-(instancetype)init
+{
+    self = [super init];
+    
+    return self;
+}
+
 @end
--- cdmx/sourceteq/source/m/stations/mstationsreadingitem.h
+++ cdmx/sourceteq/source/m/stations/mstationsreadingitem.h
@@ -1,13 +1,5 @@
-//
-//  mstationsreadingitem.h
-//  cdmx
-//
-//  Created by zero on 4/13/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import <UIKit/UIKit.h>
 
-#import <Foundation/Foundation.h>
-
-@interface mstationsreadingitem : NSObject
+@interface mstationsreadingitem:NSObject
 
 @end
