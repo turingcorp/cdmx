69fdf7b0aef14d1771407bc659526fb518cc6397
vauxhall <zero.griffin@gmail.com>
2016-04-13 10:46:25 -0500

    model index

--- cdmx/sourceteq/source/m/stations/mstationsreadinguvindex.h
+++ cdmx/sourceteq/source/m/stations/mstationsreadinguvindex.h
@@ -2,4 +2,10 @@
 
 @interface mstationsreadinguvindex:NSObject
 
++(instancetype)indexwithnumber:(NSInteger)number;
+-(instancetype)init:(NSInteger)number;
+
+@property(strong, nonatomic)UIColor *color;
+@property(nonatomic)NSInteger number;
+
 @end
--- cdmx/sourceteq/source/m/stations/mstationsreadinguvindex.m
+++ cdmx/sourceteq/source/m/stations/mstationsreadinguvindex.m
@@ -1,5 +1,46 @@
 #import "mstationsreadinguvindex.h"
+#import "mstationsreadinguvindexlow.h"
+#import "mstationsreadinguvindexmid.h"
+#import "mstationsreadinguvindexhigh.h"
+#import "mstationsreadinguvindexveryhigh.h"
+#import "mstationsreadinguvindexextreme.h"
 
 @implementation mstationsreadinguvindex
+
++(instancetype)indexwithnumber:(NSInteger)number
+{
+    mstationsreadinguvindex *index;
+    
+    if(number < 3)
+    {
+        index = [[mstationsreadinguvindexlow alloc] init:number];
+    }
+    else if(number < 6)
+    {
+        index = [[mstationsreadinguvindexmid alloc] init:number];
+    }
+    else if(number < 8)
+    {
+        index = [[mstationsreadinguvindexhigh alloc] init:number];
+    }
+    else if(number < 12)
+    {
+        index = [[mstationsreadinguvindexveryhigh alloc] init:number];
+    }
+    else
+    {
+        index = [[mstationsreadinguvindexextreme alloc] init:number];
+    }
+    
+    return index;
+}
+
+-(instancetype)init:(NSInteger)number
+{
+    self = [super init];
+    self.number = number;
+    
+    return self;
+}
 
 @end
