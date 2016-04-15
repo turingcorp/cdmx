4f139a3dcb325337338530dc112440e754b838a6
vauxhall <zero.griffin@gmail.com>
2016-04-15 14:38:35 -0500

    air cell main cell

---
+++ cdmx/sourceteq/source/v/air/vaircellmaincell.h
@@ -0,0 +1,13 @@
+//
+//  vaircellmaincell.h
+//  cdmx
+//
+//  Created by zero on 4/15/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface vaircellmaincell : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/v/air/vaircellmaincell.m
@@ -0,0 +1,13 @@
+//
+//  vaircellmaincell.m
+//  cdmx
+//
+//  Created by zero on 4/15/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "vaircellmaincell.h"
+
+@implementation vaircellmaincell
+
+@end
