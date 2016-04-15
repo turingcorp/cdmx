5554d028a607caeb29fbc5d62d8d9ed6efb24a43
vauxhall <zero.griffin@gmail.com>
2016-04-15 11:56:46 -0500

    temp asset

--- cdmx/sourceteq/source/v/air/vaircellmain.m
+++ cdmx/sourceteq/source/v/air/vaircellmain.m
@@ -2,6 +2,13 @@
 
 @implementation vaircellmain
 
-
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    [self setClipsToBounds:YES];
+    
+    return self;
+}
 
 @end
