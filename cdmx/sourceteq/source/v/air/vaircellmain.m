fb79131fbeb5ef950cf0716e4fbcd58159134579
vauxhall <zero.griffin@gmail.com>
2016-04-15 12:00:27 -0500

    icon temp

--- cdmx/sourceteq/source/v/air/vaircellmain.m
+++ cdmx/sourceteq/source/v/air/vaircellmain.m
@@ -8,6 +8,21 @@
     [self setBackgroundColor:[UIColor whiteColor]];
     [self setClipsToBounds:YES];
     
+    UIImageView *icontemp = [[UIImageView alloc] init];
+    [icontemp setClipsToBounds:YES];
+    [icontemp setUserInteractionEnabled:NO];
+    [icontemp setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [icontemp setContentMode:UIViewContentModeScaleAspectFit];
+    [icontemp setImage:[UIImage imageNamed:@"stations_temp"]];
+    
+    [self addSubview:icontemp];
+    
+    NSDictionary *views = @{@"icontemp":icontemp};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[icontemp(20)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[icontemp(30)]-10-|" options:0 metrics:metrics views:views]];
+    
     return self;
 }
 
