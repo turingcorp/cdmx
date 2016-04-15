883f0f047df628fe88636a79dafe3c38c4e4b863
vauxhall <zero.griffin@gmail.com>
2016-04-15 11:07:55 -0500

    controller air detail

---
+++ cdmx/sourceteq/source/c/air/cairdetail.h
@@ -0,0 +1,13 @@
+//
+//  cairdetail.h
+//  cdmx
+//
+//  Created by zero on 4/15/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cairdetail : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/c/air/cairdetail.m
@@ -0,0 +1,13 @@
+//
+//  cairdetail.m
+//  cdmx
+//
+//  Created by zero on 4/15/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "cairdetail.h"
+
+@implementation cairdetail
+
+@end
