19c07ded6b0a0ffe7a331784cdec6761a98b44c0
vauxhall <zero.griffin@gmail.com>
2016-04-16 12:36:58 -0500

    background image

--- cdmx/sourceteq/source/v/air/vairmap.m
+++ cdmx/sourceteq/source/v/air/vairmap.m
@@ -6,10 +6,25 @@
 {
     self = [super init];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.2]];
+    [self setBackgroundColor:[UIColor clearColor]];
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self setUserInteractionEnabled:NO];
     self.controller = controller;
+    
+    UIImageView *background = [[UIImageView alloc] init];
+    [background setUserInteractionEnabled:NO];
+    [background setClipsToBounds:YES];
+    [background setContentMode:UIViewContentModeScaleAspectFit];
+    [background setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [background setImage:[UIImage imageNamed:@"districts_cdmx"]];
+    
+    [self addSubview:background];
+    
+    NSDictionary *views = @{@"back":background};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[back]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[back]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
