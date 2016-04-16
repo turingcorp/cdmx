5420a0632d057de11471b45211347e85de9dafa6
vauxhall <zero.griffin@gmail.com>
2016-04-16 11:49:46 -0500

    model item map

--- cdmx/sourceteq/source/m/map/mcdmapitem.h
+++ cdmx/sourceteq/source/m/map/mcdmapitem.h
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
+#import "mstationsreadingitem.h"
 
 @interface mcdmapitem:NSObject
+
+@property(weak, nonatomic)mstationsreadingitem *readingmodel;
+@property(copy, nonatomic)NSString *name;
+@property(copy, nonatomic)NSString *asset;
 
 @end
