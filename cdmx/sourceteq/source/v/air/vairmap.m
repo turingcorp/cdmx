fa83e701262673a5f1ad8c77147d4f4bd3934e6a
vauxhall <zero.griffin@gmail.com>
2016-04-16 13:54:01 -0500

    animating map

--- cdmx/sourceteq/source/v/air/vairmap.m
+++ cdmx/sourceteq/source/v/air/vairmap.m
@@ -2,7 +2,8 @@
 #import "genericconstants.h"
 #import "uicolor+uicolormain.h"
 
-static NSInteger marginvr = 30;
+static CGFloat const animationaddseconds = 0.2;
+static NSInteger const marginvr = 30;
 
 @implementation vairmap
 
@@ -53,7 +54,53 @@ static NSInteger marginvr = 30;
 
 -(void)displaymap
 {
+    CGFloat height = airmapheight - (marginvr + marginvr);
     
+    UIView *overview = [[UIView alloc] init];
+    [overview setUserInteractionEnabled:NO];
+    [overview setClipsToBounds:YES];
+    [overview setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [overview setBackgroundColor:[UIColor clearColor]];
+    self.overview = overview;
+    
+    [self addSubview:overview];
+    
+    NSDictionary *views = @{@"over":overview};
+    NSDictionary *metrics = @{@"height":@(height), @"marginvr":@(marginvr)};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[over]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(marginvr)-[over(height)]" options:0 metrics:metrics views:views]];
+    
+    NSArray<mcdmapitem*> *items = self.model.items;
+    CGFloat animationseconds = animationaddseconds;
+    
+    for(mcdmapitem *item in items)
+    {
+        UIImageView *view = [[UIImageView alloc] init];
+        [view setUserInteractionEnabled:NO];
+        [view setClipsToBounds:YES];
+        [view setTranslatesAutoresizingMaskIntoConstraints:NO];
+        [view setContentMode:UIViewContentModeScaleAspectFit];
+        [view setImage:item.asset];
+        [view setAlpha:0];
+        
+        __weak typeof(view) wiew = view;
+        
+        [overview addSubview:view];
+        
+        views = @{@"view":view};
+        
+        [overview addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[view]-0-|" options:0 metrics:metrics views:views]];
+        [overview addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[view]-0-|" options:0 metrics:metrics views:views]];
+        
+        [UIView animateWithDuration:animationseconds animations:
+         ^
+         {
+             [wiew setAlpha:1];
+         }];
+        
+        animationseconds += animationaddseconds;
+    }
 }
 
 #pragma mark public
