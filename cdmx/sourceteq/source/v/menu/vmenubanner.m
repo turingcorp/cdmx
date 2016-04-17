b66d858264d4db530717a0a1a1e67cf21940d536
vauxhall <zero.griffin@gmail.com>
2016-04-16 19:31:03 -0500

    banner

--- cdmx/sourceteq/source/v/menu/vmenubanner.m
+++ cdmx/sourceteq/source/v/menu/vmenubanner.m
@@ -1,5 +1,5 @@
 #import "vmenubanner.h"
-#import "vblur.h"
+#import "uifont+uifontmain.h"
 
 @implementation vmenubanner
 
@@ -18,14 +18,35 @@
     [background setContentMode:UIViewContentModeScaleAspectFill];
     [background setTranslatesAutoresizingMaskIntoConstraints:NO];
     
-    [self addSubview:background];
+    UIImageView *logo = [[UIImageView alloc] init];
+    [logo setUserInteractionEnabled:NO];
+    [logo setClipsToBounds:YES];
+    [logo setImage:[UIImage imageNamed:@"general_logo"]];
+    [logo setContentMode:UIViewContentModeScaleAspectFit];
+    [logo setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [logo setAlpha:0.3];
     
-    NSDictionary *views = @{@"background":background};
+    UILabel *title = [[UILabel alloc] init];
+    [title setUserInteractionEnabled:NO];
+    [title setBackgroundColor:[UIColor clearColor]];
+    [title setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [title setFont:[UIFont regularsize:30]];
+    [title setTextColor:[UIColor whiteColor]];
+    [title setText:@"CDMX"];
+    [title setTextAlignment:NSTextAlignmentCenter];
+    
+    [self addSubview:background];
+    [self addSubview:logo];
+    [self addSubview:title];
+    
+    NSDictionary *views = @{@"background":background, @"logo":logo, @"title":title};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[background]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[background]-0-|" options:0 metrics:metrics views:views]];
-
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[logo]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[title]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title]-35-[logo(40)]-4-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
