bee354962fef680a9aa6e6199e39a8c2ce4f89de
vauxhall <zero.griffin@gmail.com>
2016-04-17 18:54:35 -0500

    vair geo map

--- cdmx/sourceteq/source/v/air/vairgeo.h
+++ cdmx/sourceteq/source/v/air/vairgeo.h
@@ -1,10 +1,12 @@
 #import <UIKit/UIKit.h>
 #import "cairgeo.h"
+#import "vairgeomap.h"
 
 @interface vairgeo:UIView
 
 -(instancetype)init:(cairgeo*)controller;
 
 @property(weak, nonatomic)cairgeo *controller;
+@property(weak, nonatomic)vairgeomap *map;
 
 @end
---
+++ cdmx/sourceteq/source/v/air/vairgeomap.m
@@ -0,0 +1,13 @@
+//
+//  vairgeomap.m
+//  cdmx
+//
+//  Created by zero on 4/17/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "vairgeomap.h"
+
+@implementation vairgeomap
+
+@end
