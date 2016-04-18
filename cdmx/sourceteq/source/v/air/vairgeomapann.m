209d79da5fe46b5835f1cf1f10b73cce7a9033fc
vauxhall <zero.griffin@gmail.com>
2016-04-17 21:36:24 -0500

    fixed short names according to service (need to teach those guys at government how to create apis)

--- cdmx/sourceteq/source/v/air/vairgeomapann.m
+++ cdmx/sourceteq/source/v/air/vairgeomapann.m
@@ -1,4 +1,5 @@
 #import "vairgeomapann.h"
+#import "uifont+uifontmain.h"
 
 static CGFloat const annwidth = 161;
 static CGFloat const annheight = 99;
@@ -12,32 +13,62 @@ static CGFloat const annheight = 99;
     [self setBackgroundColor:[UIColor clearColor]];
     [self setUserInteractionEnabled:NO];
     
+    NSString *stringindex = @"";
+    
+    if(annotation.index.points > 0)
+    {
+        stringindex = [NSString stringWithFormat:@"%@", @(annotation.index.points)];
+    }
+    
     UIImageView *image = [[UIImageView alloc] init];
     [image setClipsToBounds:YES];
     [image setContentMode:UIViewContentModeScaleAspectFit];
     [image setUserInteractionEnabled:NO];
     [image setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [image setImage:[UIImage imageNamed:@"stations_annotation"]];
+    [image setImage:[[UIImage imageNamed:@"stations_annotation"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    [image setTintColor:annotation.index.color];
     
     UIImageView *stick = [[UIImageView alloc] init];
     [stick setClipsToBounds:YES];
     [stick setContentMode:UIViewContentModeScaleAspectFit];
     [stick setUserInteractionEnabled:NO];
     [stick setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [stick setImage:[[UIImage imageNamed:@"stations_annotationstick"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
-    [stick setTintColor:annotation.index.color];
+    [stick setImage:[UIImage imageNamed:@"stations_annotationstick"]];
+    
+    UILabel *labeltitle = [[UILabel alloc] init];
+    [labeltitle setUserInteractionEnabled:NO];
+    [labeltitle setTextAlignment:NSTextAlignmentCenter];
+    [labeltitle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labeltitle setBackgroundColor:[UIColor clearColor]];
+    [labeltitle setTextColor:[UIColor colorWithWhite:0 alpha:0.75]];
+    [labeltitle setFont:[UIFont regularsize:12]];
+    [labeltitle setText:annotation.title];
+    
+    UILabel *labelindex = [[UILabel alloc] init];
+    [labelindex setUserInteractionEnabled:NO];
+    [labelindex setTextAlignment:NSTextAlignmentCenter];
+    [labelindex setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelindex setBackgroundColor:[UIColor clearColor]];
+    [labelindex setTextColor:[UIColor blackColor]];
+    [labelindex setFont:[UIFont regularsize:25]];
+    [labelindex setText:stringindex];
     
     [self addSubview:stick];
     [self addSubview:image];
+    [self addSubview:labeltitle];
+    [self addSubview:labelindex];
     
     CGFloat halfheight = annheight / 2.0;
-    NSDictionary *views = @{@"image":image, @"stick":stick};
+    NSDictionary *views = @{@"image":image, @"stick":stick, @"title":labeltitle, @"index":labelindex};
     NSDictionary *metrics = @{@"half":@(halfheight), @"minushalf":@(-halfheight)};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[title]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[index]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(minushalf)-[image]-(half)-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[stick]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(minushalf)-[stick]-(half)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(minushalf)-[index(40)]-(-8)-[title]" options:0 metrics:metrics views:views]];
     
     return self;
 }
