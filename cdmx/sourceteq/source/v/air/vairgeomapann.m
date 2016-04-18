12e1bba4d0420e5e5a631fb8bd3708b381afce24
vauxhall <zero.griffin@gmail.com>
2016-04-17 19:52:32 -0500

    annotation view

--- cdmx/sourceteq/source/v/air/vairgeomapann.m
+++ cdmx/sourceteq/source/v/air/vairgeomapann.m
@@ -1,13 +1,43 @@
-//
-//  vairgeomapann.m
-//  cdmx
-//
-//  Created by zero on 4/17/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "vairgeomapann.h"
+
+static CGFloat const annwidth = 25;
+static CGFloat const annheight = 48;
 
 @implementation vairgeomapann
 
+-(instancetype)init
+{
+    self = [super initWithFrame:CGRectMake(0, 0, annwidth, annheight)];
+    [self setClipsToBounds:NO];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setUserInteractionEnabled:NO];
+    
+    UIImageView *image = [[UIImageView alloc] init];
+    [image setClipsToBounds:YES];
+    [image setContentMode:UIViewContentModeScaleAspectFit];
+    [image setUserInteractionEnabled:NO];
+    [image setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [image setImage:[UIImage imageNamed:@"stations_annotation"]];
+    
+    UIImageView *stick = [[UIImageView alloc] init];
+    [stick setClipsToBounds:YES];
+    [stick setContentMode:UIViewContentModeScaleAspectFit];
+    [stick setUserInteractionEnabled:NO];
+    [stick setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [stick setImage:[UIImage imageNamed:@"stations_annotationstick"]];
+    
+    [self addSubview:stick];
+    [self addSubview:image];
+    
+    CGFloat halfheight = annheight / 2.0;
+    NSDictionary *views = @{@"image":image, @"stick":stick};
+    NSDictionary *metrics = @{@"half":@(halfheight), @"minushalf":@(-halfheight)};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(minushalf)-[image]-(half)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[stick]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(minushalf)-[stick]-(half)-|" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
 @end
