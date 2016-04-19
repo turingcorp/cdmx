1bfda556cdefc297a7495f3abaae0ff02886137e
vauxhall <zero.griffin@gmail.com>
2016-04-19 18:31:19 -0500

    notifications settings

--- cdmx/sourceteq/source/v/settings/vsettings.m
+++ cdmx/sourceteq/source/v/settings/vsettings.m
@@ -17,11 +17,19 @@
     vsettingsbar *bar = [[vsettingsbar alloc] init:controller];
     
     UILabel *title = [[UILabel alloc] init];
-    [title setText:@"Recibir notificacion"];
+    [title setText:NSLocalizedString(@"not_title", nil)];
     [title setTranslatesAutoresizingMaskIntoConstraints:NO];
     [title setUserInteractionEnabled:NO];
-    [title setFont:[UIFont regularsize:15]];
-    [title setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+    [title setFont:[UIFont regularsize:18]];
+    [title setTextColor:[UIColor colorWithWhite:0 alpha:0.8]];
+    
+    UILabel *descr = [[UILabel alloc] init];
+    [descr setText:NSLocalizedString(@"not_descr", nil)];
+    [descr setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [descr setUserInteractionEnabled:NO];
+    [descr setFont:[UIFont regularsize:14]];
+    [descr setTextColor:[UIColor colorWithWhite:0 alpha:0.6]];
+    [descr setNumberOfLines:0];
     
     UISwitch *sw = [[UISwitch alloc] init];
     [sw setTranslatesAutoresizingMaskIntoConstraints:NO];
@@ -29,17 +37,25 @@
     [sw setOn:[msettings singleton].notifications];
     
     [self addSubview:bar];
+    [self addSubview:title];
+    [self addSubview:sw];
+    [self addSubview:descr];
     
-    NSDictionary *views = @{@"bar":bar, @"title":title, @"sw":sw};
+    NSDictionary *views = @{@"bar":bar, @"title":title, @"sw":sw, @"descr":descr};
     NSDictionary *metrics = @{@"barheight":@(navbarheight)};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar(barheight)]" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[title(150)]-10-[sw]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[title(250)]-10-[sw]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[descr(250)]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[bar]-30-[sw]" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[bar]-30-[title]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[bar]-30-[title]-4-[descr]" options:0 metrics:metrics views:views]];
     
     return self;
 }
+
+#pragma mark actions
+
+
 
 @end
