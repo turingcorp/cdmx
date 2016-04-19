522895b47b0831585c680d5797e3dc0a93299ad5
vauxhall <zero.griffin@gmail.com>
2016-04-19 10:48:39 -0500

    view no drive

--- cdmx/sourceteq/source/c/cars/cnodrive.m
+++ cdmx/sourceteq/source/c/cars/cnodrive.m
@@ -1,5 +1,51 @@
 #import "cnodrive.h"
+#import "vair.h"
+#import "cmain.h"
+#import "cairgeo.h"
+#import "cairdetail.h"
 
-@implementation cnodrive
+@interface cair ()
+
+@property(strong, nonatomic)vair *view;
+
+@end
+
+@implementation cair
+
+@dynamic view;
+
+-(void)loadView
+{
+    self.view = [[vair alloc] init:self];
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    if(!self.view.collection)
+    {
+        [self.view viewappear];
+    }
+}
+
+#pragma mark public
+
+-(void)openmenu
+{
+    [[cmain singleton].pages pagemenu];
+}
+
+-(void)opengeo
+{
+    cairgeo *controller = [[cairgeo alloc] init];
+    [[cmain singleton] pushViewController:controller animated:YES];
+}
+
+-(void)selectitem:(mstationsreadingitem*)item
+{
+    cairdetail *controller = [[cairdetail alloc] init:item];
+    [self.navigationController pushViewController:controller animated:YES];
+}
 
 @end
---
+++ cdmx/sourceteq/source/v/nodrive/vnodrive.m
@@ -0,0 +1,5 @@
+#import "vnodrive.h"
+
+@implementation vnodrive
+
+@end
