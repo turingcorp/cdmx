5e5cf285ee10a1ed7af33e384140e930dbe5ed15
vauxhall <zero.griffin@gmail.com>
2016-04-18 20:40:46 -0500

    pay

--- cdmx/sourceteq/source/v/air/vairdetail.m
+++ cdmx/sourceteq/source/v/air/vairdetail.m
@@ -1,6 +1,6 @@
 #import "vairdetail.h"
 #import "vairdetailheader.h"
-
+#import "vairdetailpay.h"
 
 @implementation vairdetail
 
@@ -13,13 +13,17 @@
     
     vairdetailheader *header = [[vairdetailheader alloc] init:controller];
     
-    [self addSubview:header];
+    vairdetailpay *pay = [[vairdetailpay alloc] init];
     
-    NSDictionary *views = @{@"header":header};
+    [self addSubview:header];
+    [self addSubview:pay];
+    
+    NSDictionary *views = @{@"header":header, @"pay":pay};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[header]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[header(230)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[pay]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[header(230)]-0-[pay]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
---
+++ cdmx/sourceteq/source/v/air/vairdetailpay.m
@@ -0,0 +1,22 @@
+#import "vairdetailpay.h"
+
+@implementation vairdetailpay
+
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    
+    
+    return self;
+}
+
+-(void)drawRect:(CGRect)rect
+{
+    
+}
+
+@end
