e80d541db70ddc73462c1100f3eec31b3252de24
vauxhall <zero.griffin@gmail.com>
2016-04-18 20:26:31 -0500

    pay

---
+++ cdmx/sourceteq/source/v/air/vairdetailheaderpay.h
@@ -0,0 +1,13 @@
+//
+//  vairdetailheaderpay.h
+//  cdmx
+//
+//  Created by zero on 4/18/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface vairdetailheaderpay : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/v/air/vairdetailheaderpay.m
@@ -0,0 +1,13 @@
+//
+//  vairdetailheaderpay.m
+//  cdmx
+//
+//  Created by zero on 4/18/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "vairdetailheaderpay.h"
+
+@implementation vairdetailheaderpay
+
+@end
