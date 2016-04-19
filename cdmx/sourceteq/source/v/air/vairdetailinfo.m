c9b6c0bc0223217a1cfd1ff67afac4182969d98e
vauxhall <zero.griffin@gmail.com>
2016-04-19 09:33:11 -0500

    air detail info

--- cdmx/sourceteq/source/v/air/vairdetailinfo.m
+++ cdmx/sourceteq/source/v/air/vairdetailinfo.m
@@ -1,5 +1,6 @@
 #import "vairdetailinfo.h"
 #import "uicolor+uicolormain.h"
+#import "uifont+uifontmain.h"
 
 @implementation vairdetailinfo
 
@@ -11,19 +12,55 @@
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self setUserInteractionEnabled:NO];
     
+    NSDictionary *dicttitle = @{NSFontAttributeName:[UIFont boldsize:14], NSForegroundColorAttributeName:[UIColor colorWithWhite:0.3 alpha:1]};
+    NSDictionary *dictdescr = @{NSFontAttributeName:[UIFont regularsize:18], NSForegroundColorAttributeName:[UIColor colorWithWhite:0.6 alpha:1]};
+    
+    NSString *stringquality = model.index.name;
+    NSString *stringpollutant = model.pollutant;
+    
+    if(!stringquality)
+    {
+        stringquality = @"";
+    }
+    
+    if(!stringpollutant)
+    {
+        stringpollutant = @"";
+    }
+    
+    NSAttributedString *attrquality = [[NSAttributedString alloc] initWithString:stringquality attributes:dictdescr];
+    NSAttributedString *attrpollutant = [[NSAttributedString alloc] initWithString:stringpollutant attributes:dictdescr];
+    
+    NSAttributedString *attrtitlequality = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"air_detail_info_quality", nil) attributes:dicttitle];
+    NSAttributedString *attrtitlepollutant = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"air_detail_info_pollutant", nil) attributes:dicttitle];
+    
+    NSMutableAttributedString *mutstring = [[NSMutableAttributedString alloc] init];
+    [mutstring appendAttributedString:attrtitlequality];
+    [mutstring appendAttributedString:attrquality];
+    [mutstring appendAttributedString:attrtitlepollutant];
+    [mutstring appendAttributedString:attrpollutant];
+    
     UIView *border = [[UIView alloc] init];
     [border setUserInteractionEnabled:NO];
     [border setClipsToBounds:YES];
     [border setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.1]];
     [border setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    UILabel *label = [[UILabel alloc] init];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setUserInteractionEnabled:NO];
+    [label setNumberOfLines:0];
+    [label setAttributedText:mutstring];
+    
     [self addSubview:border];
     
-    NSDictionary *views = @{@"border":border};
+    NSDictionary *views = @{@"border":border, @"label":label};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[border(1)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[border(1)]-10-[label]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-20-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
