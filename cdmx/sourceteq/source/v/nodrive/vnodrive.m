71b78116dd65a9dd9f6262a7382798200c1dc318
vauxhall <zero.griffin@gmail.com>
2016-04-19 11:24:01 -0500

    buttron retry

--- cdmx/sourceteq/source/v/nodrive/vnodrive.m
+++ cdmx/sourceteq/source/v/nodrive/vnodrive.m
@@ -4,6 +4,8 @@
 #import "vnodriveholograms.h"
 #import "vnodriveplates.h"
 #import "mstations.h"
+#import "uifont+uifontmain.h"
+#import "uicolor+uicolormain.h"
 
 @implementation vnodrive
 
@@ -18,9 +20,24 @@
     self.bar = bar;
 
     UILabel *labelerror = [[UILabel alloc] init];
+    [labelerror setBackgroundColor:[UIColor clearColor]];
+    [labelerror setUserInteractionEnabled:NO];
+    [labelerror setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelerror setNumberOfLines:0];
+    [labelerror setFont:[UIFont regularsize:16]];
+    [labelerror setTextColor:[UIColor colorWithWhite:0 alpha:0.6]];
+    [labelerror setTextAlignment:NSTextAlignmentCenter];
     self.labelerror = labelerror;
     
     UIButton *buttontry = [[UIButton alloc] init];
+    [buttontry setClipsToBounds:YES];
+    [buttontry setBackgroundColor:[UIColor second]];
+    [buttontry setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buttontry.titleLabel setFont:[UIFont boldsize:15]];
+    [buttontry setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [buttontry setTitleColor:[UIColor colorWithWhite:1 alpha:0.2] forState:UIControlStateHighlighted];
+    [buttontry setTitle:NSLocalizedString(@"cars_nodrive_button_retry", nil) forState:UIControlStateNormal];
+    [buttontry addTarget:self action:@selector(actionretry:) forControlEvents:UIControlEventTouchUpInside];
     self.buttontry = buttontry;
     
     [self addSubview:bar];
@@ -30,9 +47,9 @@
     NSDictionary *metrics = @{};
 
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]-10-[labelerror]-10-[buttontry(34)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]-100-[labelerror(50)]-10-[buttontry(34)]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[buttontry]-50-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[labelerror]-50-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[labelerror]-20-|" options:0 metrics:metrics views:views]];
     
     [NSNotification observe:self stationsloaded:@selector(notifiedstationsloaded:)];
     
@@ -57,19 +74,41 @@
                    });
 }
 
+#pragma mark actions
+
+-(void)actionretry:(UIButton*)button
+{
+    [button setHidden:YES];
+    [[mstations singleton] fetch];
+}
+
 #pragma mark functionality
 
 -(void)tryload
 {
-    if([mstations singleton].nodrive)
+    if([mstations singleton].error || [mstations singleton].nodrive)
     {
         [self.labelerror setHidden:YES];
         [self.buttontry setHidden:YES];
+        
+        [self showcollections];
     }
     else
     {
+        [self.buttontry setHidden:NO];
         
+        NSString *stringerror = [mstations singleton].error;
+        
+        if(stringerror)
+        {
+            [self.labelerror setText:stringerror];
+        }
     }
+}
+
+-(void)showcollections
+{
+    
 }
 
 @end
