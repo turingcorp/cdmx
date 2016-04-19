9aecb65fb62d0c4ebce7b3664fc70f47ed781a3f
vauxhall <zero.griffin@gmail.com>
2016-04-19 18:03:36 -0500

    settings bar

---
+++ cdmx/sourceteq/source/v/settings/vsettingsbar.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+#import "csettings.h"
+
+@interface vsettingsbar:UIView
+
+-(instancetype)init:(csettings*)controller;
+
+@end
---
+++ cdmx/sourceteq/source/v/settings/vsettingsbar.m
@@ -0,0 +1,13 @@
+#import "vsettingsbar.h"
+
+@implementation vsettingsbar
+
+-(instancetype)init:(csettings*)controller
+{
+    self = [super init];
+    self.c
+    
+    return self;
+}
+
+@end
