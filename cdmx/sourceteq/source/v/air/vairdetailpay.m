f6de9765e39bcda4a868ef573b8e085be3a00866
vauxhall <zero.griffin@gmail.com>
2016-04-19 08:47:19 -0500

    radians

--- cdmx/sourceteq/source/v/air/vairdetailpay.m
+++ cdmx/sourceteq/source/v/air/vairdetailpay.m
@@ -1,5 +1,12 @@
 #import "vairdetailpay.h"
 
+static NSInteger const margin = 50;
+static NSInteger const linewidth = 10;
+static NSInteger const minpoints = 0;
+static NSInteger const degrees = 360;
+static CGFloat const maxpoints = 200.0;
+static CGFloat const degradian = 180.0;
+
 @implementation vairdetailpay
 
 -(instancetype)init:(mstationsreadingitemindex*)model
@@ -20,10 +27,29 @@
     CGFloat width_2 = width / 2.0;
     CGFloat height_2 = height / 2.0;
     CGFloat min = MIN(width, height);
+    CGFloat min_2 = min / 2.0;
+    CGFloat radius = min_2 - margin;
+    NSInteger points = self.model.points;
+    
+    if(points < minpoints)
+    {
+        points = minpoints;
+    }
+    else if(points > maxpoints)
+    {
+        points = maxpoints;
+    }
+    
+    CGFloat percent = points / maxpoints;
+    CGFloat degs = percent * degrees;
+    CGFloat radians = degs * M_PI / degradian;
     
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetStrokeColorWithColor(context, self.model.color.CGColor);
     CGContextSetFillColorWithColor(context, self.model.color.CGColor);
+    CGContextSetLineWidth(context, linewidth);
+    CGContextAddArc(context, width_2, height_2, radius, 0, radians, 0);
+    CGContextDrawPath(context, kCGPathStroke);
 }
 
 @end
