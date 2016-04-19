9daf2bc9bc650e0a5d54a46142d95d0170916e52
vauxhall <zero.griffin@gmail.com>
2016-04-19 16:54:20 -0500

    collection

--- cdmx/sourceteq/source/v/about/vabout.m
+++ cdmx/sourceteq/source/v/about/vabout.m
@@ -1,8 +1,15 @@
 #import "vabout.h"
 #import "vaboutbar.h"
+#import "uicolor+uicolormain.h"
+#import "vaboutcell.h"
+#import "vaboutheader.h"
 
+static NSString* const aboutheaderid = @"headerid";
+static NSString* const aboutcellid = @"cellid";
 static NSInteger const barheight = 150;
 static NSInteger const linespacing = 1;
+static NSInteger const headerheight = 100;
+static NSInteger const cellheight = 60;
 
 @implementation vabout
 
@@ -10,7 +17,7 @@ static NSInteger const linespacing = 1;
 {
     self = [super init];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor whiteColor]];
+    [self setBackgroundColor:[UIColor collection]];
     self.controller = controller;
     
     vaboutbar *bar = [[vaboutbar alloc] init:controller];
@@ -32,16 +39,59 @@ static NSInteger const linespacing = 1;
     [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
     [collection setDelegate:self];
     [collection setDataSource:self];
+    [collection registerClass:[vaboutcell class] forCellWithReuseIdentifier:aboutcellid];
+    [collection registerClass:[vaboutheader class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:aboutheaderid];
     
+    [self addSubview:collection];
     [self addSubview:bar];
     
-    NSDictionary *views = @{@"bar":bar};
+    NSDictionary *views = @{@"bar":bar, @"col":collection};
     NSDictionary *metrics = @{@"barheight":@(barheight)};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar(barheight)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar(barheight)]-0-[col]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark -
+#pragma mark col del
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout referenceSizeForHeaderInSection:(NSInteger)section
+{
+    CGFloat width = col.bounds.size.width;
+    CGSize size = CGSizeMake(width, headerheight);
+    
+    return size;
+}
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    CGFloat width = col.bounds.size.width;
+    CGSize size = CGSizeMake(width, cellheight);
+    
+    return size;
+}
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+-(UICollectionReusableView*)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+-(UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
 }
 
 @end
---
+++ cdmx/sourceteq/source/v/about/vaboutheader.m
@@ -0,0 +1,14 @@
+#import "vaboutheader.h"
+
+@implementation vaboutheader
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    return self;
+}
+
+@end
