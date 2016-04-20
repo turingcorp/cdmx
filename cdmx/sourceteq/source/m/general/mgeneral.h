1792ee0f7eee1577d48f347c19da63e975349250
vauxhall <zero.griffin@gmail.com>
2016-04-19 19:13:30 -0500

    analytics

--- cdmx/sourceteq/source/m/general/mgeneral.h
+++ cdmx/sourceteq/source/m/general/mgeneral.h
@@ -4,15 +4,28 @@
 
 typedef NS_ENUM(NSUInteger, ga_screen)
 {
-    ga_screen_stats
+    ga_screen_environment,
+    ga_screen_environment_geo,
+    ga_screen_environment_detail,
+    ga_screen_nodrive,
+    ga_screen_settings,
+    ga_screen_about,
+    ga_screen_menu
 };
 
 typedef NS_ENUM(NSUInteger, ga_event)
 {
-    ga_event_purchase
+    ga_event_environment_station,
+    ga_event_about_app,
+    ga_event_settings_notifications
 };
 
 typedef NS_ENUM(NSUInteger, ga_action)
 {
-    ga_action_optin
+    ga_action_geo,
+    ga_action_detail,
+    ga_action_review,
+    ga_action_share,
+    ga_action_on,
+    ga_action_off
 };
