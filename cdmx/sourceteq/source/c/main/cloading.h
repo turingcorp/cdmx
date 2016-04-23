8be656e757d182753aef66bd559550073aa21ead
vauxhall <zero.griffin@gmail.com>
2016-04-23 03:04:08 -0500

    removed controller loading

--- cdmx/sourceteq/source/c/main/cloading.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface cloading:UIViewController
-
-@end
--- cdmx/sourceteq/source/v/main/vloading.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "vspinner.h"
-
-@interface vloading:UIView
-
-@property(weak, nonatomic)vspinner *spinner;
-@property(weak, nonatomic)NSLayoutConstraint *layoutmargintop;
-
-@end
