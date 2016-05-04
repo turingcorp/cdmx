2ea062992cc9ded93479bf984a3aad6134393e87
vauxhall <zero.griffin@gmail.com>
2016-05-04 10:22:46 -0500

    added items to list

--- cdmx/sourceteq/source/m/pollution/mpollutiondist.m
+++ cdmx/sourceteq/source/m/pollution/mpollutiondist.m
@@ -28,9 +28,65 @@
     mpollutiondistitemcoyoacan *itemcoyoacan = [[mpollutiondistitemcoyoacan alloc] init];
     self.itemcoyoacan = itemcoyoacan;
     
+    mpollutiondistitemxochimilco *itemxochimilco = [[mpollutiondistitemxochimilco alloc] init];
+    self.itemxochimilco = itemxochimilco;
+    
+    mpollutiondistitemalvaroobregon *itemalvaroobregon = [[mpollutiondistitemalvaroobregon alloc] init];
+    self.itemalvaroobregon = itemalvaroobregon;
+    
+    mpollutiondistitemazcapozalco *itemazcapozalco = [[mpollutiondistitemazcapozalco alloc] init];
+    self.itemazcapozalco = itemazcapozalco;
+    
+    mpollutiondistitemiztapalapa *itemiztapalapa = [[mpollutiondistitemiztapalapa alloc] init];
+    self.itemiztapalapa = itemiztapalapa;
+    
+    mpollutiondistitemiztacalco *itemiztacalco = [[mpollutiondistitemiztacalco alloc] init];
+    self.itemiztacalco = itemiztacalco;
+    
+    mpollutiondistitemtlalpan *itemtlalpan = [[mpollutiondistitemtlalpan alloc] init];
+    self.itemtlalpan = itemtlalpan;
+    
+    mpollutiondistitemtlahuac *itemtlahuac = [[mpollutiondistitemtlahuac alloc] init];
+    self.itemtlahuac = itemtlahuac;
+    
+    mpollutiondistitemmilpaalta *itemmilpaalta = [[mpollutiondistitemmilpaalta alloc] init];
+    self.itemmilpaalta = itemmilpaalta;
+    
+    mpollutiondistitemmagdalenacontreras *itemmagdalenacontreras = [[mpollutiondistitemmagdalenacontreras alloc] init];
+    self.itemmagdalenacontreras = itemmagdalenacontreras;
+    
+    mpollutiondistitemgustavoamadero *itemgustavoamadero = [[mpollutiondistitemgustavoamadero alloc] init];
+    self.itemgustavoamadero = itemgustavoamadero;
+    
+    mpollutiondistitemcuajimalpa *itemcuajimalpa = [[mpollutiondistitemcuajimalpa alloc] init];
+    self.itemcuajimalpa = itemcuajimalpa;
+    
+    mpollutiondistitemcuauhtemoc *itemcuauhtemoc = [[mpollutiondistitemcuauhtemoc alloc] init];
+    self.itemcuauhtemoc = itemcuauhtemoc;
+    
+    mpollutiondistitemvenustianocarranza *itemvenustianocarranza = [[mpollutiondistitemvenustianocarranza alloc] init];
+    self.itemvenustianocarranza = itemvenustianocarranza;
+    
+    mpollutiondistitemmiguelhidalgo *itemmiguelhidalgo = [[mpollutiondistitemmiguelhidalgo alloc] init];
+    self.itemmiguelhidalgo = itemmiguelhidalgo;
+    
     self.items = @[
+                   itemalvaroobregon,
+                   itemazcapozalco,
                    itembenitojuarez,
-                   itemcoyoacan
+                   itemcoyoacan,
+                   itemcuajimalpa,
+                   itemcuauhtemoc,
+                   itemgustavoamadero,
+                   itemiztacalco,
+                   itemiztapalapa,
+                   itemmagdalenacontreras,
+                   itemmiguelhidalgo,
+                   itemmilpaalta,
+                   itemtlahuac,
+                   itemtlalpan,
+                   itemvenustianocarranza,
+                   itemxochimilco,
                    ];
     
     return self;
