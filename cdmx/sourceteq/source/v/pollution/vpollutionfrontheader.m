1e8b23eea5781aa82128cae791e4ecd1cf626d77
vauxhall <zero.griffin@gmail.com>
2016-05-04 14:15:50 -0500

    front header

--- cdmx/sourceteq/source/v/pollution/vpollutionfrontheader.m
+++ cdmx/sourceteq/source/v/pollution/vpollutionfrontheader.m
@@ -1,4 +1,7 @@
 #import "vpollutionfrontheader.h"
+#import "genericconstants.h"
+#import "ecolor.h"
+#import "efont.h"
 
 @implementation vpollutionfrontheader
 
@@ -7,6 +10,23 @@
     self = [super initWithFrame:frame];
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor clearColor]];
+    
+    CGFloat height = frame.size.height;
+    CGFloat bordery = pollution_distposy + pollution_distposy + pollution_distminsize;
+    CGFloat contentheight = height - bordery;
+    
+    UIView *bordertop = [[UIView alloc] init];
+    [bordertop setUserInteractionEnabled:NO];
+    [bordertop setBackgroundColor:[UIColor background]];
+    [bordertop setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:bordertop];
+    
+    NSDictionary *views = @{@"bordertop":bordertop};
+    NSDictionary *metrics = @{@"bordery":@(bordery)};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-5-[bordertop]-5-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V|-(bordery)-[bordertop(1)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
