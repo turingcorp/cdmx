64cc561e96a01ed96ef0338ff5404e714e59c1af
vauxhall <zero.griffin@gmail.com>
2016-05-06 13:12:32 -0500

    wording

--- cdmx/sourceteq/source/v/pollutionimeca/vpollutionimeca.m
+++ cdmx/sourceteq/source/v/pollutionimeca/vpollutionimeca.m
@@ -6,7 +6,17 @@
 {
     self = [super init:controller];
     [self.bar buttonback];
-    [self.bar title:@"IMECA"];
+    [self.bar title:NSLocalizedString(@"vpollutionimeca_title", nil)];
+    
+    UILabel *label = [[UILabel alloc] init];
+    
+    [self addSubview:label];
+    
+    NSDictionary *views = @{@"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[label]-10-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[label]" options:0 metrics:metrics views:views]];
     
     return self;
 }
