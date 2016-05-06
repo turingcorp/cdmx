4a07bea1316d590d1bae66572ddfa9a44b2b8269
vauxhall <zero.griffin@gmail.com>
2016-05-06 14:22:06 -0500

    label imeca

--- cdmx/sourceteq/source/v/pollutionimeca/vpollutionimeca.h
+++ cdmx/sourceteq/source/v/pollutionimeca/vpollutionimeca.h
@@ -5,5 +5,6 @@
 
 @property(weak, nonatomic)UIScrollView *scroll;
 @property(weak, nonatomic)UILabel *label;
+@property(strong, nonatomic)NSAttributedString *attrstring;
 
 @end
--- cdmx/sourceteq/source/v/pollutionimeca/vpollutionimeca.m
+++ cdmx/sourceteq/source/v/pollutionimeca/vpollutionimeca.m
@@ -2,16 +2,23 @@
 #import "efont.h"
 
 static NSInteger const labelmarginx = 10;
-static NSInteger const labelmargintop = 20;
-static NSInteger const labelmarginbottom = 40;
+static NSInteger const labelmargintop = 10;
+static NSInteger const labelmarginbottom = 50;
 
 @implementation vpollutionimeca
+{
+    NSInteger summarginx;
+    NSInteger summarginy;
+}
 
 -(instancetype)init:(ccontroller*)controller
 {
     self = [super init:controller];
     [self.bar buttonback];
     [self.bar title:NSLocalizedString(@"vpollutionimeca_title", nil)];
+    
+    summarginx = labelmarginx + labelmarginx;
+    summarginy = labelmargintop + labelmarginbottom;
     
     UIScrollView *scroll = [[UIScrollView alloc] init];
     [scroll setClipsToBounds:YES];
@@ -21,32 +28,44 @@ static NSInteger const labelmarginbottom = 40;
     [scroll setAlwaysBounceVertical:YES];
     self.scroll = scroll;
     
+    self.attrstring = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"vpollutionimeca_descr", nil) attributes:@{NSFontAttributeName:[UIFont regularsize:18]}];
+    
     UILabel *label = [[UILabel alloc] init];
-    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
     [label setBackgroundColor:[UIColor clearColor]];
     [label setUserInteractionEnabled:NO];
     [label setNumberOfLines:0];
-    [label setFont:[UIFont regularsize:18]];
     [label setTextColor:[UIColor colorWithWhite:0.3 alpha:1]];
-    [label setText:NSLocalizedString(@"vpollutionimeca_descr", nil)];
+    [label setAttributedText:self.attrstring];
     self.label = label;
     
     [scroll addSubview:label];
     [self addSubview:scroll];
     
-    NSDictionary *views = @{@"scroll":scroll, @"bar":self.bar, @"label":label};
-    NSDictionary *metrics = @{@"marginx":@(labelmarginx), @"margintop":@(labelmargintop), @"marginbottom":@(labelmarginbottom)};
+    NSDictionary *views = @{@"scroll":scroll, @"bar":self.bar};
+    NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[scroll]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[bar]-0-[scroll]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(marginx)-[label]-(marginx)-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(margintop)-[label]-(marginbottom)-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
 
 -(void)layoutSubviews
 {
+    CGFloat width = self.scroll.bounds.size.width;
+    CGFloat remainwidth = width - summarginx;
+    CGFloat textheight = ceilf([self.attrstring boundingRectWithSize:CGSizeMake(remainwidth, 3000) options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading context:nil].size.height);
+    CGSize size = CGSizeMake(width, textheight + summarginy);
+    CGRect frame = CGRectMake(labelmarginx, labelmargintop, remainwidth, textheight);
+    __weak typeof(self) welf = self;
+    
+    dispatch_async(dispatch_get_main_queue(),
+                   ^
+                   {
+                       [welf.scroll setContentSize:size];
+                       [welf.label setFrame:frame];
+                   });
+    
     [super layoutSubviews];
 }
 
