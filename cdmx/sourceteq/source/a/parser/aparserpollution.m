6eba34536c6c315503eba7a0fc31f0bafed9de5b
vauxhall <zero.griffin@gmail.com>
2016-05-10 16:15:54 -0500

    model hourly

--- cdmx/sourceteq/source/a/parser/aparserpollution.m
+++ cdmx/sourceteq/source/a/parser/aparserpollution.m
@@ -1,5 +1,11 @@
 #import "aparserpollution.h"
 
+@interface aparserpollution ()
+
+@property(strong, nonatomic, readwrite)NSArray<mpollutionitem*> *modelhourly;
+
+@end
+
 @implementation aparserpollution
 
 -(void)parse:(NSDictionary*)json
@@ -11,6 +17,21 @@
         NSDictionary *rawdistricts = self.validjson[@"districts"];
         NSArray *rawdaily = self.validjson[@"daily"];
         NSArray *rawhourly = self.validjson[@"hourly"];
+        NSMutableArray<mpollutionitem*> *modelhourly = [NSMutableArray array];
+        
+        NSUInteger counthours = rawhourly.count;
+        
+        for(NSUInteger indexhour = 0; indexhour < counthours; indexhour++)
+        {
+            NSDictionary *rawhour = rawhourly[indexhour];
+            NSNumber *rawhourhour = rawhour[@"hour"];
+            NSNumber *rawhourpollution = rawhour[@"pollution"];
+            
+            mpollutionitem *modelhour = [mpollutionitem pollutionhourly:rawhourhour pollution:rawhourpollution];
+            [modelhourly addObject:modelhour];
+        }
+        
+        self.modelhourly = modelhourly;
     }
 }
 
