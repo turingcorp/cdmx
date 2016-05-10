ca66cd5690fd8b419b91b79690bfa4b63fba99ab
vauxhall <zero.griffin@gmail.com>
2016-05-10 16:20:51 -0500

    parsing pollution

--- cdmx/sourceteq/source/a/parser/aparserpollution.m
+++ cdmx/sourceteq/source/a/parser/aparserpollution.m
@@ -14,10 +14,19 @@
     
     if(self.validjson)
     {
-        NSDictionary *rawdistricts = self.validjson[@"districts"];
+        NSArray *rawdistricts = self.validjson[@"districts"];
         NSArray *rawdaily = self.validjson[@"daily"];
         NSArray *rawhourly = self.validjson[@"hourly"];
         NSMutableArray<mpollutionitem*> *modelhourly = [NSMutableArray array];
+        
+        NSUInteger countdistricts = rawdistricts.count;
+        
+        for(NSUInteger indexdistricts = 0; indexdistricts < countdistricts; indexdistricts++)
+        {
+            NSDictionary *rawdist = rawdistricts[indexdistricts];
+            NSNumber *rawdistid = rawdist[@"id"];
+            NSNumber *rawdistpollution = rawdist[@"pollution"];
+        }
         
         NSUInteger counthours = rawhourly.count;
         
