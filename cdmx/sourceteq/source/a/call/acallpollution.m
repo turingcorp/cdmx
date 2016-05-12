0c73029698c3f9f57f0abeb7e60fca769fd3f64c
vauxhall <zero.griffin@gmail.com>
2016-05-12 17:22:01 -0500

    pollution api call with last stored date

--- cdmx/sourceteq/source/a/call/acallpollution.m
+++ cdmx/sourceteq/source/a/call/acallpollution.m
@@ -2,16 +2,25 @@
 #import "aparserpollution.h"
 #import "mdbselect.h"
 
+static NSString* const lastdailyparam = @"?lastdate=%@";
+
 @implementation acallpollution
 
 -(instancetype)init
 {
     self = [super init];
     
+    mdbpollutiondaily *lastdaily = [mdbselect lastpollutiondaily];
     NSDictionary *urls = [NSDictionary dictionaryWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"url" withExtension:@"plist"]];
     NSMutableString *mut = [NSMutableString string];
     [mut appendString:urls[@"server"]];
     [mut appendString:urls[@"pollution"]];
+    
+    if(lastdaily)
+    {
+        [mut appendFormat:lastdailyparam, lastdaily.date];
+    }
+    
     self.urlstring = mut;
     self.parser = [[aparserpollution alloc] init];
     
