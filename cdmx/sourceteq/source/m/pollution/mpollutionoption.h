0bad3a28978e5cee7f0aa8d14de597dbada43a57
vauxhall <zero.griffin@gmail.com>
2016-05-12 09:00:46 -0500

    model pollution option

---
+++ cdmx/sourceteq/source/m/pollution/mpollutionoption.h
@@ -0,0 +1,13 @@
+//
+//  mpollutionoption.h
+//  cdmx
+//
+//  Created by zero on 5/12/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mpollutionoption : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/m/pollution/mpollutionoption.m
@@ -0,0 +1,13 @@
+//
+//  mpollutionoption.m
+//  cdmx
+//
+//  Created by zero on 5/12/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mpollutionoption.h"
+
+@implementation mpollutionoption
+
+@end
