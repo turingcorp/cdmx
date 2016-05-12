bfc88ee3fad7baf018b820ffaf72dcd1f412d8c4
vauxhall <zero.griffin@gmail.com>
2016-05-11 19:39:30 -0500

    collection view

--- cdmx/sourceteq/source/v/pollution/vpollutioncharter.m
+++ cdmx/sourceteq/source/v/pollution/vpollutioncharter.m
@@ -3,6 +3,10 @@
 #import "ereusable.h"
 #import "vpollutioncharterheader.h"
 #import "vpollutionchartercell.h"
+#import "genericconstants.h"
+
+static NSInteger const chartercellheight = 60;
+static NSInteger const charterinteritem = -1;
 
 @implementation vpollutioncharter
 
@@ -10,7 +14,54 @@
 {
     self = [super init:controller];
     
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumInteritemSpacing:charterinteritem];
+    [flow setScrollDirection:UICollectionViewScrollDirectionVertical];
+    [flow setSectionInset:UIEdgeInsetsZero];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setBounces:NO];
+    [collection setScrollEnabled:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setDataSource:self];
+    [collection setDelegate:self];
+    [collection registerClass:[vpollutioncharterheader class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:[vpollutioncharterheader reusableidentifier]];
+    [collection registerClass:[vpollutionchartercell class] forCellWithReuseIdentifier:[vpollutionchartercell reusableidentifier]];
+    self.collection = collection;
+    
+    [self addSubview:collection];
+    
+    NSDictionary *views = @{@"col":collection};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    
     return self;
+}
+
+#pragma mark -
+#pragma mark col del
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+-(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
+{
+    return nil;
 }
 
 @end
