e85a08649198b3cb7cb342dec8208013c6704f83
vauxhall <zero.griffin@gmail.com>
2016-05-12 20:39:57 -0500

    model map

--- cdmx/sourceteq/source/m/pollutionmap/mpollutionmap.m
+++ cdmx/sourceteq/source/m/pollutionmap/mpollutionmap.m
@@ -12,12 +12,22 @@
 {
     self = [super init:model];
     
+    NSMutableArray<mpollutionmapitem*> *items = [NSMutableArray array];
     NSUInteger countdistricts = model.modeldistricts.count;
     
     for(NSUInteger indexdistrict = 0; indexdistrict < countdistricts; indexdistrict++)
     {
+        mdbdistrict *district = model.modeldistricts[indexdistrict];
+        mpollutionmapitem *model = [[mpollutionmapitem alloc] init];
+        model.index = [mpollutionindex points:district.pollution.integerValue];
+        model.name = district.name;
+        model.latitude = district.latitude;
+        model.longitude = district.longitude;
         
+        [items addObject:model];
     }
+    
+    self.items = items;
     
     return self;
 }
