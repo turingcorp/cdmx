28985e2bdd19698df39ef47687fe838ef6bad79c
vauxhall <zero.griffin@gmail.com>
2016-05-17 08:37:14 -0500

    controller pollution recommendation

---
+++ cdmx/sourceteq/source/c/pollution/cpollutionrecommendation.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+#import "ccontroller.h"
+
+@interface cpollutionrecommendation:ccontroller
+
+@end
---
+++ cdmx/sourceteq/source/c/pollution/cpollutionrecommendation.m
@@ -0,0 +1,7 @@
+#import "cpollutionrecommendation.h"
+
+@implementation cpollutionrecommendation
+
+
+
+@end
