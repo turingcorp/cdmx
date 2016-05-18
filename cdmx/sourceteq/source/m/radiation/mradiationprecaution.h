a55f766d904f5e07ce95c1a416d3754c0730e75d
vauxhall <zero.griffin@gmail.com>
2016-05-18 16:43:46 -0500

    model radiation precaution

--- cdmx/sourceteq/source/m/radiation/mradiationprecaution.h
+++ cdmx/sourceteq/source/m/radiation/mradiationprecaution.h
@@ -1,9 +1,14 @@
 #import <Foundation/Foundation.h>
+#import "mradiationprecautionlevel.h"
 
 @interface mradiationprecaution:NSObject
 
++(instancetype)umbrella:(mradiationprecautionlevel*)level;
++(instancetype)shades:(mradiationprecautionlevel*)level;
++(instancetype)sunscreen:(mradiationprecautionlevel*)level;
+
+@property(strong, nonatomic, readonly)mradiationprecautionlevel *level;
 @property(copy, nonatomic, readonly)NSString *name;
 @property(copy, nonatomic, readonly)NSString *asset;
-@property(copy, nonatomic, readonly)NSString *name;
 
 @end
