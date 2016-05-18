123fa4e0d21af4b5ef4180a79432584c5d71929c
vauxhall <zero.griffin@gmail.com>
2016-05-18 08:51:50 -0500

    climate assets

--- cdmx/sourceteq/source/v/menu/vmenucell.m
+++ cdmx/sourceteq/source/v/menu/vmenucell.m
@@ -12,7 +12,7 @@
     [label setUserInteractionEnabled:NO];
     [label setTranslatesAutoresizingMaskIntoConstraints:NO];
     [label setBackgroundColor:[UIColor clearColor]];
-    [label setFont:[UIFont boldsize:15]];
+    [label setFont:[UIFont boldsize:14]];
     [label setTextColor:[UIColor main]];
     self.label = label;
     
