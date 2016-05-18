0b982570fbd7a9d8503dc7af181d2e4dbad76dbf
vauxhall <zero.griffin@gmail.com>
2016-05-18 09:07:56 -0500

    api call radiatoion

---
+++ cdmx/sourceteq/source/a/call/acallradiation.m
@@ -0,0 +1,13 @@
+//
+//  acallradiation.m
+//  cdmx
+//
+//  Created by zero on 5/18/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "acallradiation.h"
+
+@implementation acallradiation
+
+@end
