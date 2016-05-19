7fda26c0698f5c29d2d184b143c4d5ef5c7014eb
vauxhall <zero.griffin@gmail.com>
2016-05-18 19:16:45 -0500

    controller about

---
+++ cdmx/sourceteq/source/c/radiation/cradiationabout.m
@@ -0,0 +1,13 @@
+//
+//  cradiationabout.m
+//  cdmx
+//
+//  Created by zero on 5/18/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "cradiationabout.h"
+
+@implementation cradiationabout
+
+@end
