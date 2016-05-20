cebf84dc6111eadfcb2eba9cfa263bf8c3b1c629
vauxhall <zero.griffin@gmail.com>
2016-05-20 16:14:57 -0500

    api call no drive

--- cdmx/sourceteq/source/a/call/acallnodrive.h
+++ cdmx/sourceteq/source/a/call/acallnodrive.h
@@ -1,13 +1,6 @@
-//
-//  acallnodrive.h
-//  cdmx
-//
-//  Created by zero on 5/20/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+#import "acall.h"
 
-@interface acallnodrive : NSObject
+@interface acallnodrive:acall
 
 @end
--- cdmx/sourceteq/source/a/call/acallnodrive.m
+++ cdmx/sourceteq/source/a/call/acallnodrive.m
@@ -1,13 +1,21 @@
-//
-//  acallnodrive.m
-//  cdmx
-//
-//  Created by zero on 5/20/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "acallnodrive.h"
+#import "aparsernodrive.h"
 
 @implementation acallnodrive
+
+-(instancetype)init
+{
+    self = [super init];
+    
+    NSDictionary *urls = [NSDictionary dictionaryWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"url" withExtension:@"plist"]];
+    NSMutableString *mut = [NSMutableString string];
+    [mut appendString:urls[@"server"]];
+    [mut appendString:urls[@"nodrive"]];
+    
+    self.urlstring = mut;
+    self.parser = [[aparsernodrive alloc] init];
+    
+    return self;
+}
 
 @end
