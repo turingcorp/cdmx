deb14ae93904d4f70116bc5327d2d9f95bef58d3
vauxhall <zero.griffin@gmail.com>
2016-05-22 11:00:31 -0500

    title in no drive bar

--- cdmx/sourceteq/source/v/nodrive/vnodrivebar.m
+++ cdmx/sourceteq/source/v/nodrive/vnodrivebar.m
@@ -1,6 +1,7 @@
 #import "vnodrivebar.h"
 #import "genericconstants.h"
 #import "ecolor.h"
+#import "efont.h"
 
 @implementation vnodrivebar
 
@@ -34,16 +35,36 @@
     [buttonright setImageEdgeInsets:UIEdgeInsetsMake(12, 18, 12, 0)];
     [buttonright addTarget:self action:@selector(actionbuttonright:) forControlEvents:UIControlEventTouchUpInside];
     
+    UIView *border = [[UIView alloc] init];
+    [border setBackgroundColor:[UIColor background]];
+    [border setUserInteractionEnabled:NO];
+    [border setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [border setClipsToBounds:YES];
+    
+    UILabel *label = [[UILabel alloc] init];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setUserInteractionEnabled:NO];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setFont:[UIFont boldsize:14]];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    [label setText:NSLocalizedString(@"vnodrive_title", nil)];
+    
+    [self addSubview:label];
     [self addSubview:buttonleft];
     [self addSubview:buttonright];
+    [self addSubview:border];
     
-    NSDictionary *views = @{@"buttonleft":buttonleft, @"buttonright":buttonright};
+    NSDictionary *views = @{@"buttonleft":buttonleft, @"buttonright":buttonright, @"border":border, @"label":label};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[buttonleft(60)]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[buttonright(60)]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[border(1)]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[buttonleft(45)]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[buttonright(45)]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[label(45)]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
