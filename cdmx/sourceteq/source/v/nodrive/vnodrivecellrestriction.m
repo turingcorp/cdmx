b950d63e1ba45b21e8cbed275f67a16e0df4eea4
vauxhall <zero.griffin@gmail.com>
2016-05-22 17:47:33 -0500

    no drive restriction cell

---
+++ cdmx/sourceteq/source/v/nodrive/vnodrivecellrestriction.m
@@ -0,0 +1,20 @@
+#import "vnodrivecellrestriction.h"
+#import "mnodrivetodayitemrestriction.h"
+
+@implementation vnodrivecellrestriction
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark no drive cell
+
+-(void)config:(mnodrivetodayitemrestriction*)model
+{
+}
+
+@end
