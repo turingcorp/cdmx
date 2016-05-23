b9950b4b0edb9db7c971b0aab338c97fa9bf621b
vauxhall <zero.griffin@gmail.com>
2016-05-23 08:58:35 -0500

    no drive tourist asset

---
+++ cdmx/sourceteq/source/m/nodrivetoday/mnodrivetodayitemrestrictiontourist.h
@@ -0,0 +1,13 @@
+//
+//  mnodrivetodayitemrestrictiontourist.h
+//  cdmx
+//
+//  Created by zero on 5/23/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mnodrivetodayitemrestrictiontourist : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/m/nodrivetoday/mnodrivetodayitemrestrictiontourist.m
@@ -0,0 +1,13 @@
+//
+//  mnodrivetodayitemrestrictiontourist.m
+//  cdmx
+//
+//  Created by zero on 5/23/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mnodrivetodayitemrestrictiontourist.h"
+
+@implementation mnodrivetodayitemrestrictiontourist
+
+@end
