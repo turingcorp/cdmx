e32d3ea7613432b70ec1bed6459f755b6729769e
vauxhall <zero.griffin@gmail.com>
2016-05-24 20:24:14 -0500

    controller climate

---
+++ cdmx/sourceteq/source/c/climate/cclimate.h
@@ -0,0 +1,13 @@
+//
+//  cclimate.h
+//  cdmx
+//
+//  Created by zero on 5/24/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cclimate : NSObject
+
+@end
---
+++ cdmx/sourceteq/source/c/climate/cclimate.m
@@ -0,0 +1,13 @@
+//
+//  cclimate.m
+//  cdmx
+//
+//  Created by zero on 5/24/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "cclimate.h"
+
+@implementation cclimate
+
+@end
