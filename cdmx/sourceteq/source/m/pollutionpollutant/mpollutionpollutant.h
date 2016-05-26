2d3b8d65c1678f26045ad72d76ad6ebd35bb803d
vauxhall <zero.griffin@gmail.com>
2016-05-26 09:04:18 -0500

    pollutants

--- cdmx/sourceteq/source/m/pollutionpollutant/mpollutionpollutant.h
+++ cdmx/sourceteq/source/m/pollutionpollutant/mpollutionpollutant.h
@@ -2,14 +2,10 @@
 
 @interface mpollutionpollutant:NSObject
 
-+(CGFloat)marginhr;
 +(instancetype)server:(NSNumber*)serverid;
--(void)createattributedstring:(NSString*)string attributes:(NSDictionary*)attributes width:(CGFloat)width;
 
 @property(copy, nonatomic)NSString *name;
 @property(copy, nonatomic)NSString *symbol;
 @property(copy, nonatomic)NSNumber *serverid;
-@property(strong, nonatomic, readonly)NSAttributedString *attributedstring;
-@property(assign, nonatomic)CGFloat cellheight;
 
 @end
--- cdmx/sourceteq/source/v/pollutionpollutants/vpollutionpollutants.m
+++ cdmx/sourceteq/source/v/pollutionpollutants/vpollutionpollutants.m
@@ -1,13 +1,12 @@
 #import "vpollutionpollutants.h"
 #import "cpollutionpollutants.h"
 #import "vpollutionpollutantscell.h"
-#import "vpollutionpollutantsheader.h"
 #import "ecollectioncell.h"
-#import "ecollectionreusable.h"
 #import "efont.h"
 
-static NSInteger const pollutantsheaderheight = 130;
-static NSInteger const pollutantcollectionbottom = 60;
+static NSInteger const pollutantscellheight = 60;
+static NSInteger const pollutantscollectionbottom = 40;
+static NSInteger const pollutantsinteritem = -1;
 
 @interface vpollutionpollutants ()
 
@@ -25,67 +24,12 @@ static NSInteger const pollutantcollectionbottom = 60;
     [self.bar buttonback];
     [self.bar title:NSLocalizedString(@"vpollution_pollutants_title", nil)];
     
-    return self;
-}
-
--(void)layoutSubviews
-{
-    if(!self.collection)
-    {
-        __weak typeof(self) welf = self;
-        
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
-                       ^
-                       {
-                           CGFloat width = welf.bounds.size.width;
-                           
-                           if(width)
-                           {
-                               NSDictionary *pollutants = [NSDictionary dictionaryWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"pollutants" withExtension:@"plist"]];
-                               NSDictionary *attributes = @{NSFontAttributeName:[UIFont regularsize:15]};
-                               
-                               for(mpollutionpollutant *item in welf.controller.items)
-                               {
-                                   NSNumber *pollutantid = item.serverid;
-                                   NSString *pollutantkey = [NSString stringWithFormat:@"%@", pollutantid];
-                                   NSDictionary *rawpollutant = pollutants[pollutantkey];
-                                   
-                                   if(rawpollutant)
-                                   {
-                                       NSString *symbol = rawpollutant[@"symbol"];
-                                       NSString *info = rawpollutant[@"info"];
-                                       
-                                       if(!symbol)
-                                       {
-                                           symbol = @"";
-                                       }
-                                       
-                                       [item createattributedstring:info attributes:attributes width:width];
-                                       item.symbol = symbol;
-                                   }
-                               }
-                               
-                               dispatch_async(dispatch_get_main_queue(),
-                                              ^
-                                              {
-                                                  [welf addcollection];
-                                              });
-                           }
-                       });
-    }
-    
-    [super layoutSubviews];
-}
-
-#pragma mark functionality
-
--(void)addcollection
-{
     UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
     [flow setFooterReferenceSize:CGSizeZero];
+    [flow setHeaderReferenceSize:CGSizeZero];
     [flow setMinimumInteritemSpacing:0];
-    [flow setMinimumLineSpacing:0];
-    [flow setSectionInset:UIEdgeInsetsMake(0, 0, pollutantcollectionbottom, 0)];
+    [flow setMinimumLineSpacing:pollutantsinteritem];
+    [flow setSectionInset:UIEdgeInsetsMake(0, 0, pollutantscollectionbottom, 0)];
     [flow setScrollDirection:UICollectionViewScrollDirectionVertical];
     
     UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
@@ -98,7 +42,6 @@ static NSInteger const pollutantcollectionbottom = 60;
     [collection setDataSource:self];
     [collection setDelegate:self];
     [collection registerClass:[vpollutionpollutantscell class] forCellWithReuseIdentifier:[vpollutionpollutantscell reusableidentifier]];
-    [collection registerClass:[vpollutionpollutantsheader class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:[vpollutionpollutantsheader reusableidentifier]];
     self.collection = collection;
     
     [self addSubview:collection];
@@ -108,11 +51,15 @@ static NSInteger const pollutantcollectionbottom = 60;
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[bar]-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    
+    return self;
 }
+
+#pragma mark functionality
 
 -(mpollutionpollutant*)modelforindex:(NSIndexPath*)index
 {
-    mpollutionpollutant *model = self.controller.items[index.section];
+    mpollutionpollutant *model = self.controller.items[index.item];
     
     return model;
 }
@@ -122,40 +69,22 @@ static NSInteger const pollutantcollectionbottom = 60;
 
 -(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
 {
-    mpollutionpollutant *model = [self modelforindex:index];
     CGFloat width = col.bounds.size.width;
-    CGSize size = CGSizeMake(width, model.cellheight);
-    
-    return size;
-}
-
--(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout referenceSizeForHeaderInSection:(NSInteger)section
-{
-    CGFloat width = col.bounds.size.width;
-    CGSize size = CGSizeMake(width, pollutantsheaderheight);
+    CGSize size = CGSizeMake(width, pollutantscellheight);
     
     return size;
 }
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
-    NSInteger count = self.controller.items.count;
-    
-    return count;
+    return 1;
 }
 
 -(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
 {
-    return 1;
-}
-
--(UICollectionReusableView*)collectionView:(UICollectionView*)col viewForSupplementaryElementOfKind:(NSString*)kind atIndexPath:(NSIndexPath*)index
-{
-    mpollutionpollutant *model = [self modelforindex:index];
-    vpollutionpollutantsheader *header = [col dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:[vpollutionpollutantsheader reusableidentifier] forIndexPath:index];
-    [header config:model];
+    NSInteger count = self.controller.items.count;
     
-    return header;
+    return count;
 }
 
 -(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
