07c42ec133527be876ab414b3f5a763044a44f48
vauxhall <zero.griffin@gmail.com>
2016-05-27 17:49:20 -0500

    api parser climate

--- cdmx/sourceteq/source/m/climate/mclimate.h
+++ cdmx/sourceteq/source/m/climate/mclimate.h
@@ -1,13 +1,5 @@
-//
-//  mclimate.h
-//  cdmx
-//
-//  Created by zero on 5/25/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-@interface mclimate : NSObject
+@interface mclimate:NSObject
 
 @end
--- cdmx/sourceteq/source/m/climate/mclimate.m
+++ cdmx/sourceteq/source/m/climate/mclimate.m
@@ -1,13 +1,12 @@
-//
-//  mclimate.m
-//  cdmx
-//
-//  Created by zero on 5/25/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mclimate.h"
 
 @implementation mclimate
+
+-(instancetype)init:(NSDictionary*)dictionary
+{
+    self = [super init];
+    
+    return self;
+}
 
 @end
