5c7f68c4922c3b4cce149faca5e73b8bdfc2be8e
zero <zero.griffin@gmail.com>
2016-05-30 13:05:06 -0500

    Update devices_tests.sh

--- scripts/devices_tests.sh
+++ scripts/devices_tests.sh
@@ -5,10 +5,10 @@ set -o pipefail && xcodebuild -workspace cdmx.xcworkspace \
                               -scheme cdmx \
                               -sdk "iphonesimulator" \
                               -destination "platform=iOS Simulator,OS=8.1,name=iPhone 5" \
-                              test -freshInstall -freshSimulator | xcpretty -c \
+                              test | xcpretty -c \
 
 set -o pipefail && xcodebuild -workspace cdmx.xcworkspace \
                               -scheme cdmx \
                               -sdk "iphonesimulator" \
                               -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6" \
-                              test -freshInstall -freshSimulator | xcpretty -c \
+                              test | xcpretty -c \
