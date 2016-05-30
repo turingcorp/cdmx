107c69f489233cae49232654a846f014d75ef593
vauxhall <zero.griffin@gmail.com>
2016-05-30 17:34:46 -0500

    added test for parsing stations

--- tests/admin/testaparseradminecobici.m
+++ tests/admin/testaparseradminecobici.m
@@ -27,12 +27,28 @@
     self.adminecobicimockup = nil;
 }
 
--(void)testparsestations
+-(void)testparseallstations
 {
     [self.parser parse:self.adminecobicimockup];
     NSInteger totalstations = self.parser.stations.count;
     
     XCTAssertEqual(totalstations, 50, @"Didn't parse all the stations");
+}
+
+-(void)testparsestation
+{
+    NSArray *jsonasarray = (NSArray*)self.adminecobicimockup;
+    NSDictionary *jsonelement = jsonasarray[0];
+    madminecobicistation *station = [[madminecobicistation alloc] init:jsonelement];
+    NSInteger stationid = station.stationid.integerValue;
+    NSInteger stationnamelength = station.name.length;
+    
+    XCTAssertGreaterThan(stationid, 0, @"Error parsing station id");
+    XCTAssertGreaterThan(stationnamelength, 0, @"Error parsing station name");
+    XCTAssertNotNil(station.latitude, @"Error parsing latitude");
+    XCTAssertNotNil(station.longitude, @"Error parsing longitude");
+    XCTAssertNotEqual(station.latitude.floatValue, 0, @"Latitude parsing wrong");
+    XCTAssertNotEqual(station.longitude.floatValue, 0, @"Longitude parsing wrong");
 }
 
 @end
