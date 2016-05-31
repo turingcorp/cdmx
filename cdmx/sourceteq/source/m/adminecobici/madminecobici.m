2527688d509471624c9a98b2b61e0769ceeff62a
vauxhall <zero.griffin@gmail.com>
2016-05-31 11:59:09 -0500

    refactor admin ecobici

--- cdmx/sourceteq/source/m/adminecobici/madminecobici.m
+++ cdmx/sourceteq/source/m/adminecobici/madminecobici.m
@@ -25,4 +25,24 @@
     self.stations = compiled;
 }
 
+-(NSArray<mdbecobicistation*>*)dbmodel
+{
+    NSMutableArray<mdbecobicistation*> *array = [NSMutableArray array];
+    NSInteger countstations = self.stations.count;
+    
+    for(NSUInteger indexstation = 0; indexstation < countstations; indexstation++)
+    {
+        madminecobicistation *model = self.stations[indexstation];
+        mdbecobicistation *dbmodel = [[mdbecobicistation alloc] init];
+        dbmodel.stationid = model.stationid;
+        dbmodel.name = model.name;
+        dbmodel.latitude = model.latitude;
+        dbmodel.longitude = model.longitude;
+        
+        [array addObject:dbmodel];
+    }
+    
+    return array;
+}
+
 @end
