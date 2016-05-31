fb6bca3c1add3803b57c28558b896793be16ecd8
vauxhall <zero.griffin@gmail.com>
2016-05-31 09:22:01 -0500

    ecobici menu collection

--- cdmx/sourceteq/source/v/ecobici/vecobicimenu.m
+++ cdmx/sourceteq/source/v/ecobici/vecobicimenu.m
@@ -30,15 +30,58 @@ static NSInteger const ecobicimenucellwidth = 60;
     [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
     
     UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setScrollEnabled:NO];
+    [collection setBounces:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setDelegate:self];
+    [collection setDataSource:self];
+    [collection registerClass:[vecobicimenucell class] forCellWithReuseIdentifier:[vecobicimenucell reusableidentifier]];
     self.collection = collection;
     
     [self addSubview:collection];
     
+    NSDictionary *views = @{@"col":collection};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    
     return self;
+}
+
+#pragma mark private
+
+-(mecobicimenuitem*)modelforindex:(NSIndexPath*)index
+{
+    mecobicimenuitem *model = self.model.items[index.item];
+    
+    return model;
 }
 
 #pragma mark -
 #pragma mark col del
+
+-(UIEdgeInsets)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout insetForSectionAtIndex:(NSInteger)section
+{
+    CGFloat width = col.bounds.size.width;
+    CGFloat cellswidth = self.model.items.count * ecobicimenucellwidth;
+    CGFloat remain = width - cellswidth;
+    UIEdgeInsets insets = UIEdgeInsetsMake(0, 0, 0, remain);
+    
+    return insets;
+}
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    CGFloat height = col.bounds.size.height;
+    CGSize size = CGSizeMake(ecobicimenucellwidth, height);
+    
+    return size;
+}
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
@@ -54,7 +97,9 @@ static NSInteger const ecobicimenucellwidth = 60;
 
 -(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
 {
+    mecobicimenuitem *model = [self modelforindex:index];
     vecobicimenucell *cell = [col dequeueReusableCellWithReuseIdentifier:[vecobicimenucell reusableidentifier] forIndexPath:index];
+    [cell config:model];
     
     return cell;
 }
--- cdmx/sourceteq/source/v/ecobici/vecobicimenucell.h
+++ cdmx/sourceteq/source/v/ecobici/vecobicimenucell.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
+#import "mecobicimenuitem.h"
 
 @interface vecobicimenucell:UICollectionViewCell
+
+-(void)config:(mecobicimenuitem*)model;
 
 @end
