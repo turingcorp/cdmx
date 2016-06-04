8156dd679fe954ac04f6f91395b8ab37d61ae3f3
vauxhall <zero.griffin@gmail.com>
2016-06-04 15:15:08 -0500

    added test for db stations

--- cdmx/tests/db/testmdbselect.m
+++ cdmx/tests/db/testmdbselect.m
@@ -7,4 +7,23 @@
 
 @implementation testmdbselect
 
+-(void)testecobicistations
+{
+    NSArray<mdbecobicistation*> *stations = [mdbselect ecobicistations];
+    
+    for(mdbecobicistation *station in stations)
+    {
+        CGFloat latitude = station.latitude.floatValue;
+        CGFloat longitude = station.longitude.floatValue;
+        
+        XCTAssertNotNil(station.name, @"Station without a name");
+        XCTAssertNotNil(station.primarykey, @"Station without a primarykey");
+        XCTAssertNotNil(station.stationid, @"Station without a stationid");
+        XCTAssertNotNil(station.latitude, @"Station without a latitude");
+        XCTAssertNotNil(station.longitude, @"Station without a longitude");
+        XCTAssertNotEqual(latitude, 0, @"Wrong station latitude");
+        XCTAssertNotEqual(longitude, 0, @"Wrong station longitude");
+    }
+}
+
 @end
