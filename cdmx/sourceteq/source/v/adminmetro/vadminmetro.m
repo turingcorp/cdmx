c7757db02c8b22887a176303925bfb2aefa7fc69
vauxhall <zero.griffin@gmail.com>
2016-06-05 14:48:03 -0500

    init view admin metro

--- cdmx/sourceteq/source/v/adminmetro/vadminmetro.m
+++ cdmx/sourceteq/source/v/adminmetro/vadminmetro.m
@@ -1,5 +1,23 @@
 #import "vadminmetro.h"
+#import "cadminmetro.h"
+
+@interface vadminmetro ()
+
+@property(weak, nonatomic)cadminmetro *controller;
+
+@end
 
 @implementation vadminmetro
+
+@dynamic controller;
+
+-(instancetype)init:(cadminmetro*)controller
+{
+    self = [super init:controller];
+    [self.bar title:NSLocalizedString(@"mmenu_item_adminmetro", nil)];
+    [self.bar buttonmenu];
+    
+    return self;
+}
 
 @end
