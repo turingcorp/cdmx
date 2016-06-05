430e2252857aafa32c3af75a34093e8397dac766
vauxhall <zero.griffin@gmail.com>
2016-06-05 14:34:07 -0500

    added item admin metro

---
+++ cdmx/sourceteq/source/m/menu/mmenusectionitemadminmetro.m
@@ -0,0 +1,31 @@
+#import "mmenusectionitemadminmetro.h"
+
+@interface mmenusectionitem ()
+
+@property(copy, nonatomic, readwrite)NSString *name;
+@property(copy, nonatomic, readwrite)NSString *asset;
+
+@end
+
+@implementation mmenusectionitemadminmetro
+
+-(instancetype)init
+{
+    self = [super init];
+    self.name = NSLocalizedString(@"mmenu_item_adminecobici", nil);
+    self.asset = @"generic_logo";
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark item
+
+-(UIViewController*)controller
+{
+    UIViewController *controller = [[UIViewController alloc] init];
+    
+    return controller;
+}
+
+@end
